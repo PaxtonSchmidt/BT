d4a364a0354e89456b4bbe541ba7b204e8e03cad
PaxtonSchmidt <67126427+PaxtonSchmidt@users.noreply.github.com>
2022-04-19 22:45:28 -0400

    Added test component for walking skeleton

---
+++ client/src/testComponent.tsx
@@ -0,0 +1,30 @@
+import React, {useState, useEffect} from 'react';
+
+function Users() {
+    const [users, setUsers] = useState<any[]>([]);
+
+    useEffect(() => {
+        fetch('/users/getUsers')
+            .then((res => {
+                if(res.ok) {
+                    return res.json();
+                }
+            }))
+            .then(jsonRes => setUsers(jsonRes));
+    }, []) //empty array passed as second argument to useEffect can be used to tell the hook to run at least once
+    console.log(users);
+
+ 
+     
+    return (
+        <>
+            {users.map((user) =>
+                <li key={user.user_id}>{user.username}</li>
+            )}
+        </>
+    )
+}
+
+    
+
+export default Users;
