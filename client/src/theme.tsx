b58fb8fec32895663549dfa75296a87fa18338ac
PaxtonSchmidt <67126427+PaxtonSchmidt@users.noreply.github.com>
2022-05-04 19:58:33 -0400

    Added a Skeleton Material UI Custom Theme

---
+++ client/src/theme.tsx
@@ -0,0 +1,21 @@
+import { createTheme } from "@mui/material/styles";
+
+export const theme = createTheme({
+    typography: {
+        fontFamily: 
+            'Lucida Sans, Lucida Sans Regular, Lucida Grande, Lucida Sans Unicode, Geneva, Verdana, sans-serif', 
+        
+        
+    },
+    palette: {
+        primary: {
+            main: '#1a1a1a'
+        },
+        secondary: {
+            main: '#efff0a'
+        },
+        info: {
+            main: '#efff0a'
+        }
+    },
+})
