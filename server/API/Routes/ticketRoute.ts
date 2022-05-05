5f2893886e04956fd99cb201aad70c48b2431a6b
PaxtonSchmidt <67126427+PaxtonSchmidt@users.noreply.github.com>
2022-05-05 16:01:02 -0400

    Server is now receiving the request data from the client

--- server/API/Routes/ticketRoute.ts
+++ server/API/Routes/ticketRoute.ts
@@ -4,6 +4,6 @@ let router = express.Router();
 let ticketRoute = require("../Controllers/ticketController");
 
 router.get("/getTickets", ticketRoute.getTickets);
-router.get("/addTicket", ticketRoute.addTicket);
+router.post("/addTicket", ticketRoute.addTicket);
 
 module.exports = router;
