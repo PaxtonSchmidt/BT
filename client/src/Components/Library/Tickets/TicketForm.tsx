b525d3807c5e760ef30216c4ea2835ae7a68ea73
PaxtonSchmidt <67126427+PaxtonSchmidt@users.noreply.github.com>
2022-05-05 12:15:05 -0400

    Adjusted wording in UI

--- client/src/Components/Library/Tickets/TicketForm.tsx
+++ client/src/Components/Library/Tickets/TicketForm.tsx
@@ -22,67 +22,111 @@ export default function TicketForm() {
                         console.log(data);
                     }}
                 >
-                {({values, handleChange, handleBlur, handleSubmit}) => (
-                    <form onSubmit={handleSubmit} onBlur={handleBlur}>
-                        <div  className='formContainer'>
-                        <div style={{display: 'flex', justifyContent: 'space-between'}} >
-
-                            <TextField
-                            label='Title' 
-                            type='text' 
-                            value={values.title}     
-                            onChange={handleChange} 
-                            onBlur={handleBlur}
-                            className='formComponent' 
-                            name='title'
-                            variant='standard'
-                            color='info'
-                            required
-                            />
-
-                            <TextField name='project' defaultValue={''} label='Project' className='formComponent' select color='info' variant='standard' onChange={handleChange} onBlur={handleBlur} sx={{width: '200px'}}>
-                            {projects.map((project, index) => {
-                            return(
-                                <MenuItem key={index} value={project} id='project' >{project}</MenuItem>
-                            );
-                            })}
-                            </TextField>
-
-                            <TextField name='user' defaultValue={''} label='Assign' className='formComponent' select color='info' variant='standard' onChange={handleChange} onBlur={handleBlur} sx={{width: '200px'}}>
-                            {users.map((user, index) => {
-                            return(
-                                <MenuItem key={index} value={user} id='user' >{user}</MenuItem>
-                            );
-                            })}
-                            </TextField>
-
-                            <TextField name='priority' defaultValue={''} label='Priority' className='formComponent' select color='info' variant='standard' onChange={handleChange} onBlur={handleBlur} sx={{width: '200px'}}>
-                            {priority.map((priority, index) => {
-                            return(
-                                <MenuItem key={index} value={priority} id='priority' >{priority}</MenuItem>
-                            );
-                            })}
-                            </TextField>
-                        </div>
-                        <div className='formSection' >
-                            <TextField
-                                label='Description' 
-                                type='text' 
-                                value={values.description} 
-                                onChange={handleChange} 
-                                onBlur={handleBlur} 
-                                className='formComponent formTextArea' 
-                                sx={{ color: '#ffffff' }}
-                                name='description'
-                                variant='standard'
-                                color='info'
-                                multiline
-                                required/>
-                        </div>
-                        </div>
-                            <button type='submit' name='submit' className='button bottomButtons' style={{borderRadius: '5px 0px 5px 5px', margin: '2px 0px 0px 0px', float: 'right'}}>Submit</button>
-                    </form>
-                )}
+                {({values, handleChange, handleBlur, handleSubmit, handleReset}) => {
+                        return (
+                            <form onSubmit={handleSubmit} onBlur={handleBlur}>
+                            <div className='formContainer'>
+                                <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                                    <TextField
+                                        label='Title'
+                                        type='text'
+                                        value={values.title}
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        className='formComponent'
+                                        name='title'
+                                        variant='standard'
+                                        color='info'
+                                        required />
+                                    <TextField select 
+                                        name='priority' 
+                                        defaultValue={''} 
+                                        value={values.priority} 
+                                        label='Priority' 
+                                        className='formComponent' 
+                                        color='info' 
+                                        variant='standard' 
+                                        onChange={handleChange} 
+                                        onBlur={handleBlur} 
+                                        sx={{ width: '200px' }}>
+                                        {priority.map((priority, index) => {
+                                            return (
+                                                <MenuItem key={index} value={priority} id='priority'>{priority}</MenuItem>
+                                            );
+                                        })}
+                                    </TextField>
+                                    <TextField select 
+                                        name='project' 
+                                        defaultValue={''} 
+                                        value={values.project}  
+                                        label='Project' 
+                                        className='formComponent' 
+                                        color='info' 
+                                        variant='standard' 
+                                        onChange={handleChange} 
+                                        onBlur={handleBlur} 
+                                        sx={{ width: '200px' }}
+                                        required>
+                                        {projects.map((project, index) => {
+                                            return (
+                                                <MenuItem key={index} value={project} id='project'>{project}</MenuItem>
+                                            );
+                                        })}
+                                    </TextField>
+                                    <TextField select  
+                                        name='user' 
+                                        defaultValue={''} 
+                                        value={values.user} 
+                                        label='Assignee' 
+                                        className='formComponent' 
+                                        color='info' 
+                                        variant='standard' 
+                                        onChange={handleChange} 
+                                        onBlur={handleBlur} 
+                                        sx={{ width: '200px' }}>
+                                        {users.map((user, index) => {
+                                            return (
+                                                <MenuItem key={index} value={user} id='user'>{user}</MenuItem>
+                                            );
+                                        })}
+                                    </TextField>
+                                </div>
+                                <div className='formSection'>
+                                    <TextField
+                                        label='Description'
+                                        type='text'
+                                        value={values.description}
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        className='formComponent formTextArea'
+                                        sx={{ color: '#ffffff' }}
+                                        name='description'
+                                        variant='standard'
+                                        color='info'
+                                        multiline
+                                        required />
+                                </div>
+                            </div>
+                            <div style={{ width: '100%', display: 'flex', justifyContent: 'right' }}>
+                                <button type='reset'
+                                    onClick={handleReset}
+                                    className='button bottomButtons cancelButton'
+                                    style={{ margin: '2px 2px 0px 0px' }}>
+                                    X
+                                </button>
+                                <button type='submit'
+                                    name='submit'
+                                    className='button bottomButtons submitButton' 
+                                    style={{
+                                        borderRadius: '5px 0px 5px 5px',
+                                        margin: '2px 0px 0px 0px'
+                                    }}>
+                                    Submit
+                                </button>
+                            </div>
+                            </form>
+                        );
+                    }}
                 </Formik>
             </div>
         </ThemeProvider>
