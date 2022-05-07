f1052da88c84260188ac0d32c80bd78491c3705c
PaxtonSchmidt <67126427+PaxtonSchmidt@users.noreply.github.com>
2022-05-07 15:34:32 -0400

    Added new Routes to app, team and project endpoints

--- server/app.ts
+++ server/app.ts
@@ -1,12 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const jwt = require('jsonwebtoken');
 
 app.use(express.json());
 app.use(cors());
-app.use("/users/", require("./API/Routes/userRoute"));
-app.use("/tickets/", require("./API/Routes/ticketRoute"));
+app.use('/', require('./API/Routes/AuthRoutes/authenticationRoute'));
+app.use('/users/', require('./API/Routes/userRoute'));
+app.use('/tickets/', require('./API/Routes/ticketRoute'));
+app.use('/teams/', require('./API/Routes/teamRoute'));
+app.use('/projects/', require('./API/Routes/projectRoute'))
 
 app.listen('4000', () => {
     console.log('server started on port 4000');
