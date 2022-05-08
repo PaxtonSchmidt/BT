3edfa421663033229748b3743b8ea69a8ab07a4d
PaxtonSchmidt <67126427+PaxtonSchmidt@users.noreply.github.com>
2022-05-07 21:58:31 -0400

    added Add a user to a team form

--- client/src/Components/Layout/Pages/ManageTeamPage/ManageTeamPage.tsx
+++ client/src/Components/Layout/Pages/ManageTeamPage/ManageTeamPage.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import InviteToTeamForm from '../../../Library/Forms/inviteToTeamForm';
 
 export default function Dashboard() {
     return(
         <div className='pageContentContainer'>
             <div className='pageHeaderContainer'>
                 <h1 className='pageHeader'>TEAM</h1>
+                <Container className='pageBodyContainer3'>
+                    <InviteToTeamForm /> 
+                </Container>
             </div>
         </div>
     )
