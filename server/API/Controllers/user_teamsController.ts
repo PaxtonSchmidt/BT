4218ffdd430cfa7098c149be7d683454b5f8f74c
PaxtonSchmidt <67126427+PaxtonSchmidt@users.noreply.github.com>
2022-05-07 21:59:49 -0400

    implemented most basic form of adding a user to a team via a user_teams table

--- server/API/Controllers/user_teamsController.ts
+++ server/API/Controllers/user_teamsController.ts
@@ -1,6 +1,7 @@
 import {connectionPool} from '../dbConnectionPool';
 
 function createUserTeamsTable(req: any, res: any) {
+    
     let sql ="CREATE TABLE user_teams(user_id INT(11) NOT NULL, team_id INT(11) NOT NULL, role_id INT(1) NOT NULL, date_joined DATETIME NOT NULL, enlisted_by_user_id INT(11) NOT NULL, PRIMARY KEY(user_id, team_id))";
 
     connectionPool.query(sql, (err: Error, result: any) => {
@@ -10,4 +11,17 @@ function createUserTeamsTable(req: any, res: any) {
     });
 }
 
-module.exports = {createUserTeamsTable}
+function addUserToTeam(req: any, res: any) {
+    console.log('got here')
+    let user = {user_id: req.body.invitee, team_id: '1', role_id: '1', date_joined: '2022-05-07', enlisted_by_user_id: '1'}
+
+    let sql='INSERT INTO user_teams SET ?'
+
+    connectionPool.query(sql, user, (err: Error, result: any) => {
+        if(err) throw err;
+        console.log(result);
+        res.send('User added to team...');
+    });
+}
+
+module.exports = { createUserTeamsTable, addUserToTeam }
