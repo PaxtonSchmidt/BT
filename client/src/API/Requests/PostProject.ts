8194df7602cbecbdcc84dfc1e0afd02a9017b74c
PaxtonSchmidt <67126427+PaxtonSchmidt@users.noreply.github.com>
2022-05-07 21:58:00 -0400

    post request for projects

---
+++ client/src/API/Requests/PostProject.ts
@@ -0,0 +1,13 @@
+import { Project } from "../interfaces/project";
+
+export default async function postProject(data: Project) {
+    const response = await fetch('/projects/addProject', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    console.log(response.status);
+    // return response.json().then((response) => console.log(response));
+}
