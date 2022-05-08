8fa4d19f99fa921b737040fa1222f7498c5dbe34
PaxtonSchmidt <67126427+PaxtonSchmidt@users.noreply.github.com>
2022-05-07 22:01:03 -0400

    un-de-resegregated user_teams table route from teams table routes

--- server/API/Routes/user_teamsRoute.ts
+++ server/API/Routes/user_teamsRoute.ts
@@ -1,11 +1,7 @@
 import * as express from 'express';
 let router = express.Router();
-
-let teamRoute = require("../Controllers/teamController");
 let userTeamsRoute = require("../Controllers/user_teamsController");
 
-router.get("/addTeam", teamRoute.addTeam);
-
-router.get("/addUserToTeam", userTeamsRoute.addUserToTeam);
+router.post("/addUserToTeam", userTeamsRoute.addUserToTeam);
 
 module.exports = router;
