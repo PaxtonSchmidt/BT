8b6e4185203952401b8c23eaf3f17ec758032b0d
PaxtonSchmidt <67126427+PaxtonSchmidt@users.noreply.github.com>
2022-05-07 21:58:39 -0400

    added a project form

--- client/src/Components/Layout/Pages/ProjectsPage/ProjectsPage.tsx
+++ client/src/Components/Layout/Pages/ProjectsPage/ProjectsPage.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import ProjectForm from '../../../Library/Forms/ProjectForm';
 
 export default function ProjectsPage() {
     return(
         <div className='pageContentContainer'>
             <div className='pageHeaderContainer'>
                 <h1 className='pageHeader'>PROJECTS</h1>
+                <Container className='pageBodyContainer3'>
+                <ProjectForm />
+                </Container>
             </div>
         </div>
     )
