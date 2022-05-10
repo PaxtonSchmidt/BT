0ed2b6ac669083fdcdd95e2bb58aeb0573b80335
PaxtonSchmidt <67126427+PaxtonSchmidt@users.noreply.github.com>
2022-05-10 16:46:23 -0400

    Nested private routes, next make login route to redirect to

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,6 @@
 import { ThemeProvider } from '@mui/material';
 import { Route, Routes } from 'react-router-dom';
-import Hamburger from './Components/Layout/NavigationComponents/Hamburger';
-import Navbar from './Components/Layout/NavigationComponents/Navbar/Navbar';
-import Sidebar from './Components/Layout/NavigationComponents/Sidebar/Sidebar';
+import NavigationSuite from './Components/Layout/NavigationComponents/NavigationSuite';
 import DashboardPage from './Components/Layout/Pages/DashboardPage/DashboardPage';
 import ManageTeamPage from './Components/Layout/Pages/ManageTeamPage/ManageTeamPage';
 import ProjectsPage from './Components/Layout/Pages/ProjectsPage/ProjectsPage';
@@ -14,16 +12,14 @@ function App() {
   
   return (
     <ThemeProvider theme={theme}>
-      <div className='body'>
-        <Navbar />
-        <Hamburger />
-        <Sidebar />
-
+      <div className='app'>
         <Routes>
-          <Route path='/' element={<DashboardPage />} />
-          <Route path='tickets' element={<TicketsPage />} />
-          <Route path='projects' element={<ProjectsPage />} />
-          <Route path='team' element={<ManageTeamPage />} />
+          <Route path='/' element={<NavigationSuite /> }>
+            <Route path='dashboard' element={<DashboardPage />} />
+            <Route path='tickets' element={<TicketsPage />} />
+            <Route path='projects' element={<ProjectsPage />} />
+            <Route path='team' element={<ManageTeamPage />} />
+          </Route>
         </Routes>
       </div>
     </ThemeProvider>
