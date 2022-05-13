1ee5bfbb513aa41c63f98e086403f5f288962e08
PaxtonSchmidt <67126427+PaxtonSchmidt@users.noreply.github.com>
2022-05-13 12:46:50 -0400

    Got rid of some trash

--- client/src/API/Context/AuthContext.ts
+++
@@ -1,4 +0,0 @@
-import { createContext } from "react";
-import { AuthContext } from "../interfaces/AuthContext";
-
-let AuthContext = createContext<AuthContext>(null!);
--- client/src/API/interfaces/AuthContext.ts
+++
@@ -1,7 +0,0 @@
-import { User } from "./User";
-
-export interface AuthContext {
-    user: any;
-    signin: (user: string, callback: VoidFunction) => void;
-    signout: (callback: VoidFunction) => void;
-}
