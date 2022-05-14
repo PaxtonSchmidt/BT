07ec7b0e7878c574b88ef0faa33fe04e8605b809
PaxtonSchmidt <67126427+PaxtonSchmidt@users.noreply.github.com>
2022-05-13 21:06:31 -0400

    Flushed out login form, added redirect on login and navigate away buttons

--- client/src/Components/Library/Forms/LoginForm.tsx
+++ client/src/Components/Library/Forms/LoginForm.tsx
@@ -6,19 +6,35 @@ import { authService } from '../../../API/Services/AuthService';
 import { Claims } from '../../../API/interfaces/claims';
 import { Dispatch } from 'react';
 import { SetStateAction } from 'react';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useNavigate } from 'react-router-dom';
+import gitHub from '../../Images/Icons/github.svg';
 
 interface Props {
     setIsLoggedIn: Dispatch<SetStateAction<boolean>>
 }
 
+
+
 export default function LoginForm({ setIsLoggedIn }: Props) {
+    let navigate = useNavigate();
+
+    function handleSignUpClick() {
+        navigate('../SignUp');
+    }
+    function handleDemoClick() {
+        navigate('../DemoApp');
+    }
+    function handleGitHubClick() {
+        window.location.href= 'https://github.com/PaxtonSchmidt/BT';
+    }
+
     async function handleSubmit(data: Claims) {
             console.log(data); 
             let attemptResult = await authService.signIn(data);
             console.log(attemptResult)
             if(attemptResult === 200){
                 setIsLoggedIn(true)
+                navigate('/dashboard')
             }
     }
 return(
@@ -55,27 +71,48 @@ return(
                                 onChange={handleChange} 
                                 onBlur={handleBlur} 
                                 required />
-                            <div style={{marginTop: '20px'}}>
-                                <button className='button'>sign up</button>
-                                <button className='button'>demo login</button>
-                                <button className='button'>github</button>
+                            
+                        </div>
+                        <div style={{ width: 'fit-content', display: 'inline-flex', float: 'right' }}>
+                            
+                            <div>
+                                <button type='submit'
+                                    name='login'
+                                    className='button bottomButtons submitButton scaleYonHover' 
+                                    style={{
+                                        borderRadius: '5px 0px 5px 5px',
+                                        margin: '2px 0px 0px 0px',
+                                        justifySelf: 'right'
+                                    }}>
+                                    Sign in
+                                </button>
                             </div>
                         </div>
-                        <div style={{ width: '100%', display: 'flex', justifyContent: 'right' }}>
-                            <button type='submit'
-                                name='login'
-                                className='button bottomButtons submitButton' 
-                                style={{
-                                    borderRadius: '5px 0px 5px 5px',
-                                    margin: '2px 0px 0px 0px'
-                                }}>
-                                Sign in
-                            </button>
-                        </div>
                         </form>
+                        
                     )}
                 }
             </Formik>
+            <div style={{float: 'left'}}>
+                <button 
+                    onClick={handleSignUpClick}
+                    className='button bottomButtons scaleYonHover' 
+                    style={{margin: '2px 2px 0px 0px', borderRadius: '0px 5px 5px 5px'}}>
+                    Sign Up
+                </button>
+                <button 
+                    onClick={handleDemoClick}
+                    className='button bottomButtons scaleYonHover' 
+                    style={{margin: '2px 2px 0px 0px'}}>
+                    Demo
+                </button>
+                <button 
+                    onClick={handleGitHubClick}
+                    className='button bottomButtons scaleYonHover' 
+                    style={{margin: '2px 2px 0px 0px'}}>
+                    <img src={gitHub} alt='go to code'/>
+                </button>
+            </div>
         </div>
     )
 }
