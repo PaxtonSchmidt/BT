9561b08280f33451b95edd5e6a62e44673b18238
PaxtonSchmidt <67126427+PaxtonSchmidt@users.noreply.github.com>
2022-05-20 17:45:09 -0400

    consumeCookieService now has error handling for incorrectly passed dataNeeded flags

--- server/API/Services/consumeCookie.ts
+++ server/API/Services/consumeCookie.ts
@@ -3,10 +3,15 @@ const jwt_decode = require('jwt-decode');
 export default function consumeCookie(cookie: any, dataNeeded: string) {
     let lengthOfTokenKeyInCookie = 6;
     let token = cookie.substring(lengthOfTokenKeyInCookie);
-    if(dataNeeded === 'needEntireToken') {
+
+    let entireTokenFlag = 'needEntireToken';
+    let tokenUserIdFlag = 'needTokenUser_id';
+    if(dataNeeded === entireTokenFlag) {
         return token
-    } else if(dataNeeded === 'needTokenUser_id') {
+    } else if(dataNeeded === tokenUserIdFlag) {
         let decoded = jwt_decode(token);
         return decoded.user_id;
+    } else {
+        console.log(`ERROR: Incorrect use of consumeCookie service function. Use ${entireTokenFlag} or ${tokenUserIdFlag} in the second argument`);
     }
 }
