4e09c8206a9a077140ee4a4bcfed479ac981a2d3
PaxtonSchmidt <67126427+PaxtonSchmidt@users.noreply.github.com>
2022-05-19 23:36:35 -0400

    visiting the login in page sets isTeamSelected to false

--- client/src/Components/Layout/Pages/LoginPages/LoginPage.tsx
+++ client/src/Components/Layout/Pages/LoginPages/LoginPage.tsx
@@ -1,7 +1,8 @@
-import react, { Dispatch } from 'react';
+import react, { Dispatch, useEffect } from 'react';
 import { SetStateAction } from 'react';
 import { Container } from 'react-bootstrap';
 import { boolean } from 'yup';
+import { authService } from '../../../../API/Services/AuthService';
 import LoginForm from '../../../Library/Forms/LoginForm';
 
 interface Props {
@@ -9,6 +10,11 @@ interface Props {
 }
 
 export default function LoginPage({ setIsLoggedIn }: Props) {
+    //if(isLoggedIn)return(<h1>we have you logged in</h1><button>log out?</button>) else {->}
+    useEffect(() => {
+        setIsLoggedIn(false); 
+        authService.signOut()}
+    , [])
     return(
         <Container  className='pageBodyContainer3' 
                     style={{width: '100%',  
