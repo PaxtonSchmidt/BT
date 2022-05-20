6b7ee27132d3b824e55b8fa1b1022cf579734a19
PaxtonSchmidt <67126427+PaxtonSchmidt@users.noreply.github.com>
2022-05-20 17:46:20 -0400

    Adding a table will now call a subQuery that will add the submitting user to a user_team with the new team in the db. Uses result.insertId

--- server/API/Controllers/teamController.ts
+++ server/API/Controllers/teamController.ts
@@ -1,6 +1,7 @@
 import {connectionPool} from '../dbConnectionPool';
 import authenticateJWT from '../Services/authenticateJWT';
 import consumeCookie from '../Services/consumeCookie';
+import getCurrentTime from '../Services/getCurrentTime';
 
 function createTeamsTable(req: any, res: any) {
     let sql ="CREATE TABLE teams(team_id INT(11) NOT NULL AUTO_INCREMENT, name varchar(50) NOT NULL, date_created DATETIME NOT NULL, creator_user_id INT(11), PRIMARY KEY(team_id))";
@@ -13,18 +14,26 @@ function createTeamsTable(req: any, res: any) {
 }
 
 function addTeam(req: any, res: any) {
-    let ownerUserId = consumeCookie(req.headers.cookie, 'needTokenUser_id')
-    let team = {name: req.body.name, date_created: '2022-05-19', owner_user_id: ownerUserId}
- 
-    //THIS MAY BE CREATING A NEW TEAM FIELD, BUT IT ALSO MUST CREATE A NEW FIELD OF USER_TEAMS 
-    //FOR THE THE OWNER
+    let dateTime = getCurrentTime();
+    let creatorUserId = consumeCookie(req.headers.cookie, 'needTokenUser_id');
+    let newTeamId = ''
 
-    let sql = "INSERT INTO teams SET ?"; 
-
-    connectionPool.query(sql, team, (err: any, result: any) => {
+    //THIS NOW ADDS A FIELD IN USER TEAMS FOR THE OWNER OF THE NEWLY CREATED TEAM
+    let team = {name: req.body.name, date_created: dateTime, owner_user_id: creatorUserId}
+    let insertTeamSql = "INSERT INTO teams SET ?;"; 
+    connectionPool.query(insertTeamSql, team, (err: any, result: any) => {
         if (err) result.send(err);
+
+        let user_teamValues = {user_id: creatorUserId, team_id: result.insertId, role_id: '1', date_joined: dateTime, enlisted_by_user_id: creatorUserId }
+        let user_teamSQL = "INSERT INTO user_teams SET ?"
+        connectionPool.query(user_teamSQL, user_teamValues, (err: any, result: any) => {
+            if (err) result.send(err);
+            res.send(result.status)
+        })
         res.send(result.status);
     })
+
+    console.log(newTeamId)
 }
 
 
