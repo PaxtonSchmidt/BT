4cec0947b1854909d41b837bd00870a8a79f86e9
PaxtonSchmidt <67126427+PaxtonSchmidt@users.noreply.github.com>
2022-05-19 23:38:52 -0400

    All protected routes now use authenticateRequest middleware

--- server/app.ts
+++ server/app.ts
@@ -1,3 +1,5 @@
+import authenticateRequest from "./API/Middleware/Authentication/authenticateRequest";
+
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -6,12 +8,25 @@ app.use(express.json());
 app.use(cors());
 app.use('/', require('./API/Routes/AuthRoutes/authenticationRoute'));
 app.use('/signup/', require('./API/Routes/signUpRoute'));
-app.use('/users/', require('./API/Routes/userRoute'));
-app.use('/tickets/', require('./API/Routes/ticketRoute'));
-app.use('/teams/', require('./API/Routes/teamRoute'));
-app.use('/projects/', require('./API/Routes/projectRoute'));
-app.use('/manageTeam/', require('./API/Routes/user_teamsRoute'));
 
+
+//Protected routes have Auth/Auth middlewares
+app.use('/teams/', authenticateRequest,
+    require('./API/Routes/teamRoute'));
+
+app.use('/users/', authenticateRequest,
+    require('./API/Routes/userRoute'));
+
+app.use('/tickets/', authenticateRequest,
+    require('./API/Routes/ticketRoute'));
+
+app.use('/projects/', authenticateRequest,
+    require('./API/Routes/projectRoute'));
+
+app.use('/manageTeam/', authenticateRequest,
+    require('./API/Routes/user_teamsRoute'));
+
+    
 app.listen('4000', () => {
     console.log('server started on port 4000');
 })
