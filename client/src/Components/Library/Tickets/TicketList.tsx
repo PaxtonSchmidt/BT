fb088c221ae4220133e1fd9bd848ef29d2130e63
PaxtonSchmidt <67126427+PaxtonSchmidt@users.noreply.github.com>
2022-05-22 17:25:03 -0400

    dont need column titles, people are smart and they looked bad

--- client/src/Components/Library/Tickets/TicketList.tsx
+++ client/src/Components/Library/Tickets/TicketList.tsx
@@ -28,46 +28,20 @@ function Tickets() {
             <h4 className='button'>Written</h4>
         </div>
         <div className='list' style={{textAlign: 'left'}}>
-            <div className='listItemContainer'>
-                <div className='listItem' style={{color: '#efff0a'}}>
-                    <span className='listColumnNames'>
-                        Title
-                    </span>
-                    <span className='listColumnNames'>
-                        Project
-                    </span>
-                    <span className='listColumnNames'>
-                        Assigned
-                    </span>
-                    <span className='listColumnNames'>
-                        Priority
-                    </span>
-                    <span className='listColumnNames'>
-                        Status
-                    </span>
-                    <span className='listColumnNames'>
-                        Created
-                    </span>
-                    <span className='listColumnNames'>
-                        
-                    </span>
-                </div>
-                
-                    {tickets.map((ticket) =>
-                    <TicketListItem 
-                    key={ticket.ticket_id}
-                    ticketID={ticket.ticket_id}
-                    authorID={ticket.author_id}
-                    relevantProjectID={ticket.relevant_project_id}
-                    title={ticket.title}
-                    descripton={ticket.description}
-                    dateCreated={ticket.date_created}
-                    dateLastUpdated={ticket.date_last_updated}
-                    assignedUserID={ticket.assigned_user_id}
-                    resolutionStatus={ticket.resolution_status}
-                    priority={ticket.priority} />
-                )}
-            </div>
+                {tickets.map((ticket) =>
+                <TicketListItem 
+                key={ticket.ticket_id}
+                ticketID={ticket.ticket_id}
+                authorID={ticket.author_id}
+                relevantProjectID={ticket.relevant_project_id}
+                title={ticket.title}
+                descripton={ticket.description}
+                dateCreated={ticket.date_created}
+                dateLastUpdated={ticket.date_last_updated}
+                assignedUserID={ticket.assigned_user_id}
+                resolutionStatus={ticket.resolution_status}
+                priority={ticket.priority} />
+            )}
         </div>
     </Container>
         
