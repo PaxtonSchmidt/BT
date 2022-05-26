3491c5a8f74b79ce34b8ad711dd5907841ecba99
PaxtonSchmidt <67126427+PaxtonSchmidt@users.noreply.github.com>
2022-05-26 15:56:35 -0400

    cleaned out log

--- server/API/Middleware/authenticateRequest.ts
+++ server/API/Middleware/authenticateRequest.ts
@@ -2,9 +2,9 @@ import authenticateJWT from "../Services/authenticateJWT";
 
 export default function authenticateRequest(req: any, res: any, next: any) {
     if(authenticateJWT(req.headers.cookie) === true){
-        console.log('ayo')
         next()
     } else{
-        res.sendStatus(401);
+        console.log('bad request')
+        res.sendStatus(400);
     }
 }
