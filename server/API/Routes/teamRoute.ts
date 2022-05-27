0e218864477f34ebfb65e610489bf0c66abbd720
PaxtonSchmidt <67126427+PaxtonSchmidt@users.noreply.github.com>
2022-05-27 19:01:45 -0400

    consolidated extra routes

--- server/API/Routes/teamRoute.ts
+++ server/API/Routes/teamRoute.ts
@@ -3,8 +3,15 @@ let router = express.Router();
 
 let teamRoute = require("../Controllers/teamController");
 let teamSelectRoute = require("../Models/Pages/TeamSelect");
+let teamAuthorization = require('../Requests/Authorization/teamAuthorization')
+
 
 router.post("/addTeam", teamRoute.addTeam);
 router.get("/getTeams", teamSelectRoute.getCurrentUserTeams);
+
+router.get('getTeamIvites', teamRoute.getTeamInvites)
+router.post("/inviteUserToTeam", teamAuthorization.inviteUserToTeam);
+router.post('/acceptInviteToTeam', teamAuthorization.acceptInviteToTeam);
+
 
 module.exports = router;
