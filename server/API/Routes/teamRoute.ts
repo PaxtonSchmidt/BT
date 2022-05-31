3cb35dd954bff14d3478cdd4cfc0bdfd29b65c15
PaxtonSchmidt <67126427+PaxtonSchmidt@users.noreply.github.com>
2022-05-31 14:58:29 -0400

    corrected routes

--- server/API/Routes/teamRoute.ts
+++ server/API/Routes/teamRoute.ts
@@ -2,14 +2,14 @@ import * as express from 'express';
 let router = express.Router();
 
 let teamRoute = require("../Controllers/teamController");
-let teamSelectRoute = require("../Models/Pages/TeamSelect");
+let teamSelectRoute = require("../Requests/Authorization/NoAuthorization/TeamSelect");
 let teamAuthorization = require('../Requests/Authorization/teamAuthorization')
 
 
 router.post("/addTeam", teamRoute.addTeam);
 router.get("/getTeams", teamSelectRoute.getCurrentUserTeams);
+router.get('/getTeamInvites', teamSelectRoute.getTeamInvites)
 
-router.get('getTeamIvites', teamRoute.getTeamInvites)
 router.post("/inviteUserToTeam", teamAuthorization.inviteUserToTeam);
 router.post('/acceptInviteToTeam', teamAuthorization.acceptInviteToTeam);
 
