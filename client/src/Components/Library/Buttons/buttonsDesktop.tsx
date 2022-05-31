d869457919eadbef380f01c61fdd48fc71c0640c
PaxtonSchmidt <67126427+PaxtonSchmidt@users.noreply.github.com>
2022-05-31 14:55:14 -0400

    component now consume invites array redux state

--- client/src/Components/Library/Buttons/buttonsDesktop.tsx
+++ client/src/Components/Library/Buttons/buttonsDesktop.tsx
@@ -1,12 +1,32 @@
-import e from 'express';
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import { bindActionCreators } from 'redux';
 import { authService } from '../../../API/Services/AuthService';
+import { Invites } from '../../../Redux/interfaces/invites';
+import { InvitesActionCreators } from '../../../Redux';
 
 export default function SelectTeamPageButtons(){
+    const invitesState = useSelector((state: Invites) => state.invites)
+    const dispatch = useDispatch();
+    const { update } = bindActionCreators(InvitesActionCreators, dispatch)
     let navigate = useNavigate();
-    let invites = '4';
-    let plurality = isPlural(invites);
+
+    useEffect(() => {
+        fetch('/teams/getTeamInvites')
+        .then((res => {
+            if(res.ok) {         
+                return res.json();
+            } else if(res.status === 404){
+                return res.json()
+            } 
+        }))
+        .then(jsonRes => update(jsonRes));
+    }, [])  
+
+    let inviteAmount = invitesState.length;
+
+    let plurality = isPlural(invitesState.length);
     //get the amount of team invites the user has from invite store
     function isPlural(invites: any){
         if(invites === '1'){
@@ -34,8 +54,8 @@ export default function SelectTeamPageButtons(){
             <button className='selectTeamPageNavbutton' onClick={handleLogout}>Logout</button>
             <button className='selectTeamPageNavbutton' style={{bottom: '30px'}} onClick={handleCreateTeam}>Create a Team</button>
             <button className='selectTeamPageNavbutton' style={{bottom: '50px'}} onClick={handleCheckInvites}>
-                <span className='invitesNum'>{invites}</span>
-                {plurality}</button>
+                <span className='invitesNum'>{inviteAmount}</span>{plurality}
+            </button>
         </>
     )
 
