fac23d7b08a70b613f153ac1e0574fc899729869
PaxtonSchmidt <67126427+PaxtonSchmidt@users.noreply.github.com>
2022-05-31 00:12:09 -0400

    Added dynamic loading for nav buttons in select team page, still need to make mobile buttons

--- client/src/Components/Layout/Pages/LoginPages/SelectTeamPage.tsx
+++ client/src/Components/Layout/Pages/LoginPages/SelectTeamPage.tsx
@@ -1,25 +1,32 @@
-import react, { Dispatch, useEffect } from 'react';
+import react, { Dispatch } from 'react';
 import { SetStateAction } from 'react';
-import { Container } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
-import { Navigate, useNavigate } from 'react-router-dom';
-import { authService } from '../../../../API/Services/AuthService';
+import { Navigate } from 'react-router-dom';
 import { State } from '../../../../Redux/reducers';
+import SelectTeamPageButtons from '../../../Library/Buttons/buttonsDesktop';
 import TeamList from '../../../Library/Teams/TeamList';
 
 interface Props {
     setIsTeamSelected: Dispatch<SetStateAction<boolean>>
 }
 
+// function decideButtons() {
+//     if(window.innerWidth > 720){
+//         return <ButtonsDesktop />
+//     } else {
+//         return <ButtonsModile />
+//     }
+// }
+
+
 export default function SelectTeamPage({ setIsTeamSelected }: Props) {
     const loginState = useSelector((state: State) => state.login)
-
-    
     
     if(loginState === 1) {
         return(
-            <div className='teamCardPageBody'>
-                <div id='cards'>
+            <div className='teamCardPageBody altBG'>
+                <SelectTeamPageButtons />
+                <div id='cards'>                 
                     <TeamList setIsTeamSelected={setIsTeamSelected} />
                 </div>
             </div>
