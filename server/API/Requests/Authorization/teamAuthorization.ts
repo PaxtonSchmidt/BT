3042a1402a01ec8a27360c7b17d80ba0ba5c45b2
PaxtonSchmidt <67126427+PaxtonSchmidt@users.noreply.github.com>
2022-05-31 00:16:00 -0400

    All auth logic is in auth level, no more logic in controllers. return when sending http headers

--- server/API/Requests/Authorization/teamAuthorization.ts
+++ server/API/Requests/Authorization/teamAuthorization.ts
@@ -1,18 +1,26 @@
 import consumeCookie from "../../Services/consumeCookies/consumeCookie";
 import { consumeCookieFlags } from "../../Services/consumeCookies/consumeCookieFlags";
-
+import { consumeRowDataPacket } from "../../Services/consumeRowDataPacket";
 let authorizationController = require('../../Controllers/AuthControllers/authorizationController')
 let teamController = require('../../Controllers/teamController')
 let Roles = require('./Roles')
+let users = require('../../Controllers/userController')
+let teams = require('../../Controllers/teamController')
 
 async function inviteUserToTeam(req: any, res: any) {
     let userTeamIDCombo = consumeCookie(req.headers.cookie, consumeCookieFlags.tokenUserAndTeamIdFlag);
-    let currentUserRoleID = await authorizationController.fetchUserTeamRoleID(req, res, userTeamIDCombo);
+    let currentUserRoleID = await authorizationController.fetchUserTeamRoleID(req, userTeamIDCombo);
 
-    if(currentUserRoleID === Roles.Legend.owner){
-        await teamController.addTeamInvite(req, res, userTeamIDCombo)
+    let recipientID = await users.getUserByNameDiscriminator(req.body.invitee, req.body.discriminator, res)
+    let isInviteExisting = consumeRowDataPacket(await teams.getInviteByUserIDRecipientIDTeamID(userTeamIDCombo.userID, recipientID, userTeamIDCombo.teamID, res))
+
+
+    if(currentUserRoleID !== Roles.Legend.owner){
+        return res.status(401).send({message: "You can't invite people to this team..."})
+    } if(isInviteExisting){
+        return res.status(400).send({message: 'Invite already exists...'});
     } else {
-        res.sendStatus(401)
+        teamController.addTeamInvite(req, res, userTeamIDCombo, recipientID)
     }
 }
 
