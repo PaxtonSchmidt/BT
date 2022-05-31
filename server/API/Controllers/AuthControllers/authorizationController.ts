8ed17e6ee04b0f55e5c259f1da10ae7beb050875
PaxtonSchmidt <67126427+PaxtonSchmidt@users.noreply.github.com>
2022-05-31 00:15:01 -0400

    didnt need response object

--- server/API/Controllers/AuthControllers/authorizationController.ts
+++ server/API/Controllers/AuthControllers/authorizationController.ts
@@ -3,7 +3,7 @@ import consumeCookie from "../../Services/consumeCookies/consumeCookie";
 import { consumeCookieFlags } from "../../Services/consumeCookies/consumeCookieFlags";
 
 
-function fetchUserTeamRoleID(req: any, res: any, userTeamIDCombo: any) {
+function fetchUserTeamRoleID(req: any, userTeamIDCombo: any) {
     let variables = [userTeamIDCombo.userID, userTeamIDCombo.teamID]
     let sql = 'SELECT role_id FROM user_teams WHERE user_id= ? AND team_id= ?';
     return new Promise<any>((resolve, reject) => {
