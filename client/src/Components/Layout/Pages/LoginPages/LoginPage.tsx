7f14cc606e9bcff7f9d635a9f425c9ecb3d7db8f
PaxtonSchmidt <67126427+PaxtonSchmidt@users.noreply.github.com>
2022-06-03 18:08:10 -0400

    now logs out in redux state too

--- client/src/Components/Layout/Pages/LoginPages/LoginPage.tsx
+++ client/src/Components/Layout/Pages/LoginPages/LoginPage.tsx
@@ -1,14 +1,20 @@
 import react, { Dispatch, useEffect } from 'react';
 import { SetStateAction } from 'react';
 import { Container } from 'react-bootstrap';
-import { boolean } from 'yup';
+import { useDispatch } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import { authService } from '../../../../API/Services/AuthService';
+import { LoginActionCreators } from '../../../../Redux';
 import LoginForm from '../../../Library/Forms/LoginForm';
 
 
 export default function LoginPage() {
-    //if(isLoggedIn)return(<h1>we have you logged in</h1><button>log out?</button>) else {->}
+    let dispatch = useDispatch();
+    const { logout } = bindActionCreators(LoginActionCreators, dispatch)
+
+
     useEffect(() => { 
+        logout();
         authService.signOut()
         authService.deselectTeam()}
     , [])
