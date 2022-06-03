e08640884003fba3653ba46b12f69ade8186f0cb
PaxtonSchmidt <67126427+PaxtonSchmidt@users.noreply.github.com>
2022-06-02 22:57:26 -0400

    cleaning

--- client/src/Components/Layout/Pages/LoginPages/LoginPage.tsx
+++ client/src/Components/Layout/Pages/LoginPages/LoginPage.tsx
@@ -12,6 +12,7 @@ export default function LoginPage() {
         authService.signOut()
         authService.deselectTeam()}
     , [])
+
     
     return(
         <Container  className='loginPage' >
--- client/src/Components/Layout/Pages/LoginPages/SelectTeamPage.tsx
+++ client/src/Components/Layout/Pages/LoginPages/SelectTeamPage.tsx
@@ -1,8 +1,11 @@
 import react, { Dispatch } from 'react';
 import { useEffect } from 'react';
 import { SetStateAction } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
+import { bindActionCreators } from 'redux';
+import getCurrentUser from '../../../../API/Requests/Login/GetCurrentUser';
+import { CurrentUserActionCreators } from '../../../../Redux';
 import { State } from '../../../../Redux/reducers';
 import SelectTeamPageButtons from '../../../Library/Buttons/buttonsDesktop';
 import TeamList from '../../../Library/Teams/TeamList';
@@ -23,11 +26,12 @@ interface Props {
 export default function SelectTeamPage({ setIsTeamSelected }: Props) {
     const loginState = useSelector((state: State) => state.login)
     
+    
     if(loginState === 1) {
         return(
             <div className='teamCardPageBody altBG'>
                 <SelectTeamPageButtons />
-                <div id='cards'>                 
+                <div id='cards'>       
                     <TeamList setIsTeamSelected={setIsTeamSelected} />
                 </div>
             </div>
