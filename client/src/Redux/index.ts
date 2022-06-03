d6fd2c488cc05ca995236f80e184af2fd90c8ce6
PaxtonSchmidt <67126427+PaxtonSchmidt@users.noreply.github.com>
2022-06-02 22:58:45 -0400

    currentUser store made for redux, only going to use this in login pages

--- client/src/Redux/index.ts
+++ client/src/Redux/index.ts
@@ -1,2 +1,3 @@
 export * as LoginActionCreators from './action-creators/loginActionCreators'
 export * as InvitesActionCreators from './action-creators/invitesActionCreators'
+export * as CurrentUserActionCreators from './action-creators/currentUserActionCreators'
