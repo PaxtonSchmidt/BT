3c2cf6257504b74085d833b39ff5f74a92592128
PaxtonSchmidt <67126427+PaxtonSchmidt@users.noreply.github.com>
2022-06-03 23:33:43 -0400

    manually clearing team and invite state on page login, saves trying to window.location.reload

--- client/src/Components/Layout/Pages/LoginPages/LoginPage.tsx
+++ client/src/Components/Layout/Pages/LoginPages/LoginPage.tsx
@@ -1,22 +1,34 @@
 import react, { Dispatch, useEffect } from 'react';
 import { SetStateAction } from 'react';
 import { Container } from 'react-bootstrap';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import postInvalidateJWT from '../../../../API/Requests/Login/PostInvalidateJWT';
 import { authService } from '../../../../API/Services/AuthService';
-import { LoginActionCreators } from '../../../../Redux';
+import { InvitesActionCreators, LoginActionCreators, TeamsActionCreators } from '../../../../Redux';
 import LoginForm from '../../../Library/Forms/LoginForm';
 
 
 export default function LoginPage() {
     let dispatch = useDispatch();
     const { logout } = bindActionCreators(LoginActionCreators, dispatch)
-
+    const { updateTeams } = bindActionCreators(TeamsActionCreators, dispatch)
+    const { updateInvites } = bindActionCreators(InvitesActionCreators, dispatch)
 
     useEffect(() => { 
+        //this way you cant spam for new tokens 
+        let isLoggedIn = window.sessionStorage.getItem('isLoggedIn')
+        if(isLoggedIn === 'true'){
+            postInvalidateJWT();
+        }
+        let teams: any = []
+        let invites: any = []
+        updateTeams({teams})
+        updateInvites({invites})
         logout();
         authService.signOut()
-        authService.deselectTeam()}
+        authService.deselectTeam()
+        }
     , [])
 
     
