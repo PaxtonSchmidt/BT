8e20c039062bca4ca622f5d6881e2a9bee5dff50
PaxtonSchmidt <67126427+PaxtonSchmidt@users.noreply.github.com>
2022-06-04 23:00:22 -0400

    sessionState query fully constructs sessionState object before sending it in the response

--- server/API/Controllers/Authorization/NoAuthorization/sessionState.ts
+++ server/API/Controllers/Authorization/NoAuthorization/sessionState.ts
@@ -14,40 +14,36 @@ interface sessionUser {
 async function getSessionState(req: any, res: any) {
     let sessionUser: sessionUser = {username: '', discriminator: '', bio: ''};
     let sessionTeam = {name: '', date_joined: '', team_role: '', project_roles: []};
-    let sessionProjectsRoles: any = [];
-    let invites = [{}];
-    
+    let sessionInvites = [{}];
+    let sessionState: any = {};
     let currentUserID = consumeCookie(req.headers.cookie, consumeCookieFlags.tokenUserIdFlag);
     let currentTeamID = consumeCookie(req.headers.cookie, consumeCookieFlags.tokenTeamIdFlag);
-    console.log(currentTeamID);
     
     //get session info from database queries 
     try{
         let currentUser = await user.getUserByID(currentUserID)
         sessionUser = Object.assign(sessionUser, currentUser)
-        console.log(sessionUser)
 
         let currentTeam = await team.getSessionTeam(currentTeamID, currentUserID)
         sessionTeam = Object.assign(sessionTeam, currentTeam)
 
         let userRoles = await project.getSessionProjectRoles(currentTeamID, currentUserID)
-        userRoles = userRoles.map((project: any) => Object.assign({}, project));
-        sessionTeam.project_roles = userRoles
-        console.log('a')
+        sessionTeam.project_roles = userRoles.map((project: any) => Object.assign({}, project));
 
+        let userInvites = await team.getTeamInvites(currentUserID);
+        sessionInvites = userInvites.map((invite: any) => Object.assign({}, invite));
+ 
+        sessionState = {currentUser: sessionUser, currentTeam: sessionTeam, invites: sessionInvites}
     }catch(e){
         return res.status(500).send({message: 'Server couldnt fetch user information...'})
     }
-    
-    
-    //build out sessionState object
-    try{
-        return res.status(200).send({message: 'Oh god i hope this works'})
-    }catch(e){
-        console.log('catch it')
-    }
 
-    //if something is okay send sessionStateInformation
+    
+    try{
+        return res.status(200).send(sessionState)
+    }catch(e){
+        return res.status(500).send({message: 'Server couldnt retrieve user details...'})
+    }
     
 }
 
