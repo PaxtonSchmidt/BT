cbfce259ec62a1492fbf53d517a83a6a9154b6d3
PaxtonSchmidt <67126427+PaxtonSchmidt@users.noreply.github.com>
2022-06-16 18:32:20 -0400

    Layed out the foundations for editing the users role in the project

---
+++ client/src/Components/Library/Projects/UpdateUserProjectRole.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function UpdateUserProjectRole() {
+    return (
+        <div className='memberListRowSection' style={{textAlign: 'center'}}>
+            <span className='rowItem username fadeIn  updateRoleButton scaleYonHover' style={{display: 'inline-block', width: 'fit-content'}}>
+                Update
+            </span>
+        </div>
+    )
+}
