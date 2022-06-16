b3d1d3e3890dc9feaf42aa4cee64aceae664051e
PaxtonSchmidt <67126427+PaxtonSchmidt@users.noreply.github.com>
2022-06-16 18:37:02 -0400

    added route

--- server/API/Routes/projectRoute.ts
+++ server/API/Routes/projectRoute.ts
@@ -1,10 +1,11 @@
 import * as express from 'express';
 let router = express.Router();
 
-let projectRoute = require("../Queries/projectQueries");
+let teamAuthorization = require('../Controllers/Authorization/teamAuthorization')
 let projectAuthorization = require('../Controllers/Authorization/projectAuthorization')
 
-router.post("/addProject", projectRoute.addProject);
+router.post("/addProject", teamAuthorization.addProject);
 router.get('/getProjectsStatistics', projectAuthorization.getProjectsStatistics)
+router.get('/getRelatedMemberDetails', projectAuthorization.getRelatedMemberDetails)
 
 module.exports = router;
