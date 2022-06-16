7e5ecbddc00428462c053aed67c4b8bbfe94fdad
PaxtonSchmidt <67126427+PaxtonSchmidt@users.noreply.github.com>
2022-06-16 18:31:52 -0400

    added a little onHover sugar and the discriminator style

--- client/src/Components/Library/Projects/ProjectMembersListItem.tsx
+++ client/src/Components/Library/Projects/ProjectMembersListItem.tsx
@@ -1,36 +1,39 @@
-import { dividerClasses } from '@mui/material';
-import React, {useState, useEffect} from 'react';
-import { ReactElement } from 'react';
-import { Container } from 'react-bootstrap';
+import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { FocusedTicketActionCreators } from '../../../Redux';
+import { FocusedMemberActionCreators } from '../../../Redux';
 import { State } from '../../../Redux/reducers';
-import ProjectListItem from './ProjectListItem';
+import RoleTranslator from '../../../Services/RoleTranslator'
 
-// interface Props{
+interface Props{
+    member: any;
+}
 
-// }
-
-export default function ProjectMembersListItem() {
-        return (
-            <div className='ListContainer'>
-                <div className='listItem listRow' style={{justifyContent: 'space-between'}}>
-                    <div className='memberListRowSection'>
-                        <span className='rowItem' style={{display: 'inline-block', width: 'fit-content'}}>
-                            Member Username
-                        </span>
-                        <span className='rowItem' style={{display: 'inline-block', width: 'fit-content'}}>
-                            Member Discriminator
-                        </span>
-                    </div>
-                    <div className='memberListRowSection'>
-                        <span className='rowItem'>
-                            role
-                        </span>
-                    </div>
+export default function ProjectMembersListItem(props: Props) {
+    const dispatch = useDispatch();
+    const { updateFocusedMember } = bindActionCreators(FocusedMemberActionCreators, dispatch)
+    let role = RoleTranslator(props.member.role_id)
+    
+    return (
+        <div className='ListContainer' onClick={() => updateFocusedMember(props.member)}>
+            <div className='listItem listRow memberRow' style={{justifyContent: 'space-between'}}>
+                <div className='memberListRowSection' style={{textAlign: 'center'}}>
+                    <span className='rowItem username' style={{display: 'inline-block', width: 'fit-content'}}>
+                        {props.member.username } 
+                    </span>
+                    <span className='rowItem discriminator' style={{display: 'inline-block', width: 'fit-content'}}>
+                      #{props.member.discriminator}
+                    </span>
+                    <span className='showDetailsHighlight'>
+                        {`Show details`}
+                    </span>
+                </div>
+                <div className='memberListRowSection' style={{textAlign: 'center'}}>
+                    <span className='rowItem'>
+                        {role}
+                    </span>
                 </div>
             </div>
-        )
-    }
-
+        </div>
+    )
+}
