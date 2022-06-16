dfeaefd90e347a3f2e511e6dbfbb1c7b3bdcdbca
PaxtonSchmidt <67126427+PaxtonSchmidt@users.noreply.github.com>
2022-06-16 18:30:41 -0400

    Consumes focusedProject state and dispatches it. Adds style to selected project

--- client/src/Components/Library/Projects/ProjectListItem.tsx
+++ client/src/Components/Library/Projects/ProjectListItem.tsx
@@ -1,14 +1,34 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { FocusedProjectActionCreators } from '../../../Redux';
+import { State } from '../../../Redux/reducers';
 
 interface Props{
     name: string;
-    isSelected: boolean;
 }
 
 export default function ProjectListItem(props: Props) {
+    const dispatch = useDispatch();
+    const { updateFocusedProject } = bindActionCreators(FocusedProjectActionCreators, dispatch)
+    const focusedProjectState = useSelector((state: State) => state.focusedProject)
+    let name = props.name
+    
+    function handleSelect(name: any){
+        updateFocusedProject({name: name})
+    }
+
+    if(focusedProjectState.name === props.name){
+        document.getElementById(name)?.classList.add('selectedProjectInList')
+        document.getElementById(name)?.classList.remove('scaleYonHover')
+    } else {
+        document.getElementById(name)?.classList.remove('selectedProjectInList')
+        document.getElementById(name)?.classList.add('scaleYonHover')
+    }
+
     return (
-        <div className='sideScrollListItem scaleYonHover' >
-            {props.name}
+        <div onClick={() => handleSelect(name)} className='sideScrollListItem scaleYonHover fadeIn' id={name}>
+            {name}
         </div>
     )
 }
