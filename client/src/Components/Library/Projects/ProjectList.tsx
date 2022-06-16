bcac07688153e3bf073cf351ad98c04269ff4a0a
PaxtonSchmidt <67126427+PaxtonSchmidt@users.noreply.github.com>
2022-06-16 18:30:13 -0400

    Added a toggle between showing all projects the user is in for in the project list or just the ones they Lead

--- client/src/Components/Library/Projects/ProjectList.tsx
+++ client/src/Components/Library/Projects/ProjectList.tsx
@@ -1,19 +1,47 @@
-import React, {useState, useEffect} from 'react';
-import { ReactElement } from 'react';
-import { Container } from 'react-bootstrap';
+import { userInfo } from 'os';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { FocusedTicketActionCreators } from '../../../Redux';
+import { FocusedProjectActionCreators } from '../../../Redux';
 import { State } from '../../../Redux/reducers';
 import ProjectListItem from './ProjectListItem';
 
 function ProjectList() {
+    const [filterToLeadingOnly, setFilterToLeadingOnly]= useState(false)
     const sessionState = useSelector((state: State) => state.session)
     let projectsList: any = ['All'] 
-    sessionState.currentTeam?.projects.forEach((project: any) => projectsList.push(project.name))
+    let currentUser = {
+        username: sessionState.currentUser?.username,
+        discriminator: sessionState.currentUser?.discriminator
+    }
     
-   
-   
+
+    if(filterToLeadingOnly === true){
+        sessionState.currentTeam?.projects.forEach((project: any) => {
+            let leaders: any = project.project_members.filter((member: any) => {if(member.role_id === 1 || member.role_id === 2){return true}})
+            let index = leaders.findIndex((leader: any) => {
+                if(leader.username === currentUser.username && leader.discriminator === currentUser.discriminator) {
+                    return true
+                }
+            })
+            //if the current user exists in the list of leader of each individual project, push that project onto the viewable project list for the current user
+            if(index !== -1){
+                projectsList.push(project.name)
+            }
+        })
+    } else {
+        sessionState.currentTeam?.projects.forEach((project: any) => projectsList.push(project.name))
+    }
+    
+ 
+    function checkView(){
+        if(filterToLeadingOnly === true){
+            return 'Viewing Projects You Lead'
+        } else {
+            return 'Viewing Assigned Projects'
+        }
+    }
+    let WhichView = checkView()
 
 
     if(!projectsList){
@@ -36,8 +64,9 @@ function ProjectList() {
         return (
             <div onWheel={handleWheel} id='projectList'  className='sideScrollList componentGlow'>
                 {projectsList!.map((project: any) =>
-                    <ProjectListItem isSelected={false} key={project} name={project} />
+                    <ProjectListItem key={project} name={project} />
                 )}
+                <button className='filterProjectsToggle scaleYonHover' onClick={() => setFilterToLeadingOnly(!filterToLeadingOnly)}>{WhichView}</button>
             </div>
             
         )
