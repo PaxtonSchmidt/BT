996f55c893f7f5f0453207aab832d38a7204d126
PaxtonSchmidt <67126427+PaxtonSchmidt@users.noreply.github.com>
2022-06-24 15:29:33 -0400

    added new routes for the project endpoints

--- server/API/Routes/projectRoute.ts
+++ server/API/Routes/projectRoute.ts
@@ -1,11 +1,16 @@
 import * as express from 'express';
 let router = express.Router();
-
 let teamAuthorization = require('../Controllers/Authorization/teamAuthorization')
 let projectAuthorization = require('../Controllers/Authorization/projectAuthorization')
 
 router.post("/addProject", teamAuthorization.addProject);
+
 router.get('/getProjectsStatistics', projectAuthorization.getProjectsStatistics)
 router.get('/getRelatedMemberDetails', projectAuthorization.getRelatedMemberDetails)
+router.get('/getUsersOnTeam', projectAuthorization.getUsersOnTeam)
+
+router.put('/updateMemberRole', projectAuthorization.updateMemberRole)
+
+router.delete('/removeMember', projectAuthorization.removeMember)
 
 module.exports = router;
