443a4bc86839dd6f379853c33a1c77e183bda867
PaxtonSchmidt <67126427+PaxtonSchmidt@users.noreply.github.com>
2022-06-24 15:27:15 -0400

    added functionality for changing the role of a user, this is ugly, really ugly right now

--- client/src/Components/Library/Projects/UpdateUserProjectRole.tsx
+++ client/src/Components/Library/Projects/UpdateUserProjectRole.tsx
@@ -1,11 +1,62 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { State } from '../../../Redux/reducers';
 
-export default function UpdateUserProjectRole() {
+interface Props {
+    setIsFormDirty: any;
+    isFormDirty: boolean;
+    word: string;
+    setWord: any;
+}
+
+export default function UpdateUserProjectRole(props: Props) {
+    const [isOpen, setIsOpen] = useState(false);
+    const focusedProjectState = useSelector((state: State) => state.focusedProject)
+
+    useEffect(()=>{
+        handleReset()
+    }, [focusedProjectState])
+
+    function handleReset(){
+        setIsOpen(false)
+        props.setIsFormDirty(false)
+        props.setWord('Update')
+    }
+    function handleClick(choice: any){
+        if(isOpen === true){
+            props.setWord(choice)
+            props.setIsFormDirty(true)
+        }
+        setIsOpen(!isOpen)
+    }
+
+   
     return (
-        <div className='memberListRowSection' style={{textAlign: 'center'}}>
-            <span className='rowItem username fadeIn  updateRoleButton scaleYonHover' style={{display: 'inline-block', width: 'fit-content'}}>
-                Update
-            </span>
-        </div>
+        <>
+            {isOpen ? 
+            <div style={{marginBottom: '10px'}}>
+                <span id='RoleUpdate' className='fadeIn updateRoleButton scaleYonHover' onClick={() => handleClick('Project Lead')} style={{display: 'inline-block', width: 'fit-content'}}>
+                    <div>Project Lead</div>
+                </span>
+                <span id='RoleUpdate' className='fadeIn updateRoleButton scaleYonHover' onClick={() => handleClick('Developer')} style={{display: 'inline-block', width: 'fit-content', marginLeft: '2px'}}>
+                    <div>Developer</div>
+                </span>
+                <span id='RoleUpdate' className='fadeIn updateRoleButton scaleYonHover resetRolesButton' onClick={handleReset} style={{display: 'inline-block', width: 'fit-content'}}>
+                    <div>X</div>
+                </span>
+            </div>
+            :
+            <div className='memberListRowSection'>
+                <span id='RoleUpdate' className='rowItem fadeIn updateRoleButton scaleYonHover' onClick={handleClick} style={{display: 'inline-block', width: 'fit-content', textAlign: 'center'}}>
+                    {props.word}
+                </span>
+                {props.isFormDirty ? 
+                <span id='RoleUpdate' className='updateRoleButton scaleYonHover resetRolesButton' onClick={handleReset} style={{display: 'inline-block', width: 'fit-content'}}>
+                    <div>X</div>
+                </span>
+                :<></>}
+            </div>
+            }
+        </>            
     )
 }
