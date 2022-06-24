02b950bca3bccfeaedc5672fdeedf16ddb04a1a5
PaxtonSchmidt <67126427+PaxtonSchmidt@users.noreply.github.com>
2022-06-24 15:25:23 -0400

    added removing member button and request

--- client/src/Components/Layout/NavigationComponents/Navbar/NavbarComponents/TeamAndName/TeamPic.tsx
+++ client/src/Components/Layout/NavigationComponents/Navbar/NavbarComponents/TeamAndName/TeamPic.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import TranslateTeamRole from '../../../../../../Services/RoleTranslator';
 import { State } from '../../../../../../Redux/reducers';
 
 export default function Team() {
--- client/src/Components/Layout/Pages/ProjectsPage/ProjectMembersManage.tsx
+++ client/src/Components/Layout/Pages/ProjectsPage/ProjectMembersManage.tsx
@@ -3,9 +3,10 @@ import { useDispatch, useSelector } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { FocusedMemberActionCreators } from '../../../../Redux';
 import { State } from '../../../../Redux/reducers';
-import TranslateTeamRole from '../../../../Services/RoleTranslator';
+import { translateRole } from '../../../../Services/translateRole';
 import ProjectMembersList from '../../../Library/Projects/ProjectMembersList'
-import UpdateUserProjectRole from '../../../Library/Projects/UpdateUserProjectRole';
+import UpdateUserProjectRole from '../../../Library/Projects/UpdateUserProjectRole'
+import { Box, Modal } from '@mui/material';
 let deepClone = require('lodash.clonedeep')
 
 export default function ProjectMembersManage(){
@@ -17,6 +18,8 @@ export default function ProjectMembersManage(){
     const [memberDetails, setMemberDetails] = useState<any[]>([])
     const dispatch = useDispatch();
     const { updateFocusedMember } = bindActionCreators(FocusedMemberActionCreators, dispatch)
+    const [word, setWord] = useState('Update')
+    const [isModalOpen, setIsModalOpen] = useState(false)
 
     useEffect(()  => setIsMemberSelected(!isMemberSelected), [focusedMemberState])
     useEffect(() => {  
@@ -32,14 +35,18 @@ export default function ProjectMembersManage(){
         getMemberDetails()
     },[])
 
-    let handleCloseUserDetailsForm = () => updateFocusedMember({username: '', discriminator: 0}) 
+    function handleCloseUserDetailsForm() {
+        setIsDirty(false)
+        updateFocusedMember({username: '', discriminator: 0}) 
+    }
+        
     
     if(isDirty === true){
         document.getElementById('manageUsersButton')?.classList.add('active')
     } else {
         document.getElementById('manageUsersButton')?.classList.remove('active')
     }
-    
+
     let focusedMemberDetails = memberDetails.find((member: any) => {
         if(member.username === focusedMemberState.username && member.discriminator === focusedMemberState.discriminator){
             return true
@@ -57,10 +64,9 @@ export default function ProjectMembersManage(){
     if(focusedMemberDetails !== undefined){
         if(isFocusedProjectAll){
             //since team_role is determined by your highest project_role, this works
-            role = TranslateTeamRole(focusedMemberState.role_id)
-            enlistedByUsername = 'N/A'
+            role = translateRole.TranslateRole(focusedMemberState.role_id)
+            enlistedByUsername = ''
             enlistedByDiscriminator = ''
-
         } else {
             focusedProjectOfFocusedMember = focusedMemberDetails?.memberProjects.find((member: any) => {
                 if(member.project_name === focusedProjectState.name){
@@ -71,16 +77,49 @@ export default function ProjectMembersManage(){
             })
             if(focusedProjectOfFocusedMember !== undefined){
                 let roleID = focusedProjectOfFocusedMember.project_role
-                
-                console.log(focusedProjectOfFocusedMember)
                 date = focusedProjectOfFocusedMember.date_assigned.toString().substring(0, 10)
                 enlistedByUsername = focusedProjectOfFocusedMember.assignedByUsername
                 enlistedByDiscriminator = focusedProjectOfFocusedMember.assignedByDiscriminator
-                role = TranslateTeamRole(roleID)
+                role = translateRole.TranslateProjectRole(roleID)
             } else{
                 role = `${focusedMemberState.username} is not on the ${focusedProjectState.name} project`
             }
         }
+    }
+
+    async function handleSubmit(){
+        if(word !== 'Update'){
+            let updateUser = {
+                username: focusedMemberState.username,
+                discriminator: focusedMemberState.discriminator,
+                targetProjectName: focusedProjectState.name,
+                newRoleId: translateRole.TranslateProjectRoleBack(word)
+            }
+            let response: any = fetch('/projects/updateMemberRole', {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(updateUser)
+            }).then(res => res.json())
+            return console.log(await response)
+        }
+    }
+
+    async function handleRemoveMember(){
+        let targetUserProject = {
+            username: focusedMemberState.username,
+            discriminator: focusedMemberState.discriminator,
+            project: focusedProjectState.name
+        }
+        let response: any = fetch('/projects/removeMember', {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(targetUserProject)
+        }).then(res => res.json())
+        return console.log(await response)
     }
 
     let currentUserProjects = deepClone(sessionState.currentTeam.projects)
@@ -126,7 +165,7 @@ export default function ProjectMembersManage(){
                                 </span>
                             </div>
                             {isUserAllowedToEditRole ? 
-                            <UpdateUserProjectRole />
+                            <UpdateUserProjectRole setIsFormDirty={setIsDirty} isFormDirty={isDirty} word={word} setWord={setWord}/>
                             :<></>}
                         </div>
 
@@ -148,23 +187,65 @@ export default function ProjectMembersManage(){
                             <div className='listItem listRow memberRow manageMemberListRow ' style={{justifyContent: 'space-between'}}>
                             <div className='memberListRowSection' style={{textAlign: 'left'}}>
                                 <span className='rowItem username' style={{display: 'inline-block', width: 'fit-content'}}>
-                                    Assigned to the {focusedProjectState.name} project
+                                    Joined the {focusedProjectState.name} project
                                 </span>
                                 <span className='rowItem username' style={{display: 'inline-block', width: 'fit-content'}}>
                                     {`\u00A0on\u00A0${date}`}
                                 </span>
                                 </div>
                             </div>
+                            {isUserAllowedToEditRole ?
+                            <div className='removeMemberButton' >
+                                <p style={{marginRight: '10px', cursor: 'pointer'}}>Remove</p>
+                                <svg onClick={() => setIsModalOpen(true)} xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-x-square" viewBox="0 0 16 16">
+                                    <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+                                    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+                                </svg>
+                            </div>
+                            :<></>}
+                            <Modal
+                                open={isModalOpen}
+                                style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                                    <Box className='modalBoxContainer'>
+                                        <div className='modalBox'>
+                                            <p style={{color: 'white'}} onClick={() => setIsModalOpen(false)}>
+                                                Are you sure you want to remove 
+                                                <span className='rowItem username' style={{display: 'inline-block', width: 'fit-content', color: '#efff0a'}}>
+                                                    {`\u00A0${focusedMemberState.username}`}
+                                                </span>
+                                                <span className='rowItem discriminator' style={{display: 'inline-block', width: 'fit-content', color: '#efff0a'}}>
+                                                    #{`${focusedMemberState.discriminator}\u00A0`}
+                                                </span>
+                                                from the 
+                                                <span className='rowItem' style={{display: 'inline-block', width: 'fit-content', color: '#efff0a'}}>
+                                                    {`\u00A0${focusedProjectState.name}\u00A0`}
+                                                </span>
+                                                project?
+                                            </p>
+                                            <ul className='modalList'>
+                                                <p>These effects will occur...</p>
+                                                <li>{`Tickets assigned to ${focusedMemberState.username} in the ${focusedProjectState.name} project will be set to "Unassigned"`}</li>
+                                                <li>{`${focusedMemberState.username} can no longer view the ${focusedProjectState.name} project's tickets`}</li>
+                                                <li>{`${focusedMemberState.username} can no longer make tickets for the ${focusedProjectState.name} project`}</li>
+                                                <li>{`${focusedMemberState.username} can no longer view the ${focusedProjectState.name} project's members list`}</li>
+                                            </ul>
+                                            <div style={{width: '100%', display: 'flex', justifyContent: 'space-between'}}>
+                                                <button onClick={() => setIsModalOpen(false)} className='button modalButton modalButtonCancel' >Cancel</button>
+                                                <button onClick={() => handleRemoveMember()} className='button modalButton modalButtonConfirm' >Confirm</button>
+                                            </div>
+                                        </div>
+                                        
+                                    </Box>
+                            </Modal>
                         </>
                         }
-
-                        
-                        
                     </div>
-                    <button id='manageUsersButton' className='button userDetailsButton' style={{right: '10px', bottom: '10px', position: 'absolute'}}>Submit Changes</button>       
                 </div>
             </>
             }
+        {isMemberSelected ? <></> : 
+        <button id='manageUsersButton' className='button userDetailsButton' onClick={handleSubmit}>Submit Changes</button>       
+        }
         </div>
     )
 }
