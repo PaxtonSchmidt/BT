b10a7d2f7d896bc2c184efd0cfb5030c37b0a78e
PaxtonSchmidt <67126427+PaxtonSchmidt@users.noreply.github.com>
2022-06-28 22:31:59 -0400

    few fetch request in client for POSTing new ticket comments

---
+++ client/src/API/Requests/Tickets/PostTicketComment.ts
@@ -0,0 +1,11 @@
+export default async function postTicketComment(comment: string, ticket: any) {
+    const response = await fetch('/tickets/addTicketComment', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({comment: comment, ticketID: ticket.ticket_id, projectID: ticket.project_id})
+    })
+
+    return response
+}
