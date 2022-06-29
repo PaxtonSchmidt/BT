29ff78cf9bd95ba17cf32a145b1165d684fcd3e1
PaxtonSchmidt <67126427+PaxtonSchmidt@users.noreply.github.com>
2022-06-28 22:31:42 -0400

    new fetch request in client for POSTing new project members

---
+++ client/src/API/Requests/Projects/PostNewProjectMembers.ts
@@ -0,0 +1,12 @@
+import { Teammate } from "../../interfaces/teammate";
+
+export default async function postNewProjectMembers(data: Teammate[], project: string) {
+    const response = await fetch('/projects/addListOfMembersToProject', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({newMembers: data, projectName: project})
+    })
+    return response.json()
+}
