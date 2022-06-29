6be1da010a07638f94df1335e8bf5dd57f788911
PaxtonSchmidt <67126427+PaxtonSchmidt@users.noreply.github.com>
2022-06-28 22:36:37 -0400

    added the adding multiple users to a project endpoint and the remove member from a project endpoint

--- server/API/Controllers/Authorization/projectAuthorization.ts
+++ server/API/Controllers/Authorization/projectAuthorization.ts
@@ -1,6 +1,7 @@
+import { Teammate } from '../../Interfaces/teammate';
 import composeMemberDetails from '../../Services/composeMemberDetails';
 import composeProjectStatistics from '../../Services/composeProjectStatistics';
-import consumeCookie from '../../Services/consumeCookies/consumeCookie';
+import consumeCookie, { userTeamRoleCombo } from '../../Services/consumeCookies/consumeCookie';
 import { consumeCookieFlags } from "../../Services/consumeCookies/consumeCookieFlags";
 import { consumeRowDataPacket } from '../../Services/consumeRowDataPacket';
 import {translateTicketPriority} from '../../Services/translateTicketPriority';
@@ -29,16 +30,13 @@ async function submitNewTicket(req: any, res: any) {
             targetUserId = await users.getUserByNameDiscriminator(req.body.assignee.username, req.body.assignee.discriminator, res)
             isAssigneeOnProject = consumeRowDataPacket(await projects.isUserOnProject(targetUserId.user_id, targetProjectID))
         }    
-
     }catch(e){
         return res.status(500).send({message: 'Server couldnt find role information...'})
     }
-
     //if either current user or the assigned user are not on the project, throws 403
     if(isAssigneeOnProject === false){
         return res.status(403).send({message: 'The user youre assigning is not on that project...'})
     }
-
     // if they are the relevant_team_owner they can submit tickets to this project 
     if(userTeamRoleIds.roleID === Roles.Legend.owner || isUserOnProject === true){
         try{
@@ -53,7 +51,6 @@ async function submitNewTicket(req: any, res: any) {
         return res.status(401).send({message: "You can't submit tickets to this project..."})
     }
 }
-
 async function getTickets(req: any, res: any){
     let userTeamRoleCombo: any = [];
     try{
@@ -61,7 +58,6 @@ async function getTickets(req: any, res: any){
     }catch(e){
         return res.status(500).send({message: 'Server couldnt find role information...'})
     }
-
     //if the use is the owner, send them all tickets from the team
     //if they are a dev or project lead, send them only the tickets for their assigned projects
     if(userTeamRoleCombo.roleID === Roles.Legend.owner){
@@ -82,7 +78,6 @@ async function getTickets(req: any, res: any){
         return res.status(500).send({mesage:  'Something went wrong...'})
     }
 }
-
 async function getProjectsStatistics(req: any, res: any){
     let userTeamRoleCombo: any = []
     try{
@@ -90,7 +85,6 @@ async function getProjectsStatistics(req: any, res: any){
     }catch(e){
         return res.status(500).send({message: 'Server couldnt find role information...'})
     }
-
     //if the use is the owner, send them all of the projects from the team
     //if they are a dev or project lead, send them only their assigned projects
     if(userTeamRoleCombo.roleID === Roles.Legend.owner){
@@ -117,7 +111,6 @@ async function getProjectsStatistics(req: any, res: any){
         return res.status(500).send({mesage:  'Something went wrong...'})
     }
 }
-
 async function getRelatedMemberDetails(req: any, res: any){
     let userTeamRoleCombo: any = []
     try{
@@ -125,7 +118,6 @@ async function getRelatedMemberDetails(req: any, res: any){
     }catch(e){
         return res.status(500).send({message: 'Server couldnt find role information...'})
     }
-
     //Send everyone all the project member details for projects they are in. 
     //The owner is already in every project so additional logic is not needed
     try{
@@ -136,14 +128,79 @@ async function getRelatedMemberDetails(req: any, res: any){
         return res.status(500).send({message: 'Server couldnt get Projects...'})
     }
 }
+async function addListOfMembersToProject(req: any, res: any){
+    let userTeamRoleCombo: userTeamRoleCombo = consumeCookie(req.headers.cookie, consumeCookieFlags.tokenUserTeamRoleIdFlag);
+    let newMembers: Teammate[] = req.body.newMembers;
+    let newMembersIds: number[] | null = null;
+    let projectName: string = req.body.projectName;
+    let userProjectRoleId: number | null = null;
+    let targetProjectId: number | null = null;
+    let allTeammates: any[] = [];
+    let currentProjectMembers: Teammate[] | null = null
+    //get a list of all teammates, check that these users are on the list and if they are, add them all to the project with a dev role
+    try{
+        let targetProjectIdPacket = await projects.getProjectIdByTeamIdAndProjectName(userTeamRoleCombo.teamID, projectName)
+        targetProjectId = targetProjectIdPacket.project_id
+        let userProjectRoleIdPacket = await projects.getRoleByUserIdProjectId(userTeamRoleCombo.userID, targetProjectId)
+        userProjectRoleId = userProjectRoleIdPacket[0].role_id
+        allTeammates = await teams.getUsersOnTeam(userTeamRoleCombo.teamID)
+        currentProjectMembers = await projects.getProjectMembersByProjectId(targetProjectId)
+    }catch(e){
+        return res.status(500).send({message: 'Server couldnt check authorization levels...'})
+    }
+    try{
+        async function getNewMemberIds(newMembers: Teammate[]) {
+            let ids: number[] = [];
+            //there has to be a better way, the challenge isnt getting the sql to check for many users or for multiple conditions
+            //the challenge is getting sql to check for the exact username/discriminator combo for many users
+            for(let i = 0; i < newMembers.length; i++){
+                let idPacket = await users.getUserByNameDiscriminator(newMembers[i].username, newMembers[i].discriminator)
+                ids?.push(idPacket.user_id)
+            }   
+            return ids
+        }
+        newMembersIds = await getNewMemberIds(newMembers)
+    }catch(e){
+        return res.status(500).send({message: 'Server couldnt get new member information...'})
+    }
 
+    //if the user isnt a project lead, send a 403
+    if(userProjectRoleId !== 1 && userProjectRoleId !== 2){
+        return res.status(403).send({message: `You are not allowed to add users to the ${req.body.projectName} project...`})
+    } 
+    //if any of the users arent on the team, send a 400
+    newMembers.forEach((member: Teammate) => {
+        let idx: number | null = null;
+        idx = allTeammates.findIndex((teammate: Teammate) => {
+            return member.username === teammate.username && member.discriminator === teammate.discriminator
+        })
+        if(idx === -1 || idx === null){
+            return res.status(400).send({message: 'One or more of these users are not on the team...'})
+        }
+    });
+    //if any of the users are already on the project, send a 400
+    newMembers.forEach((member: Teammate) => {
+        let idx: number | null = null;
+        idx = currentProjectMembers!.findIndex((teammate: Teammate) => {
+            return member.username === teammate.username && member.discriminator === teammate.discriminator
+        })
+        if(idx !== -1){
+            return res.status(400).send({message: 'One or more of these users are already in the project...'})
+        }
+    });
+    try{
+        await projects.addListOfUsersToProject(newMembersIds, targetProjectId, userTeamRoleCombo.teamID, userTeamRoleCombo.userID)
+        return res.status(200).send({message: 'Server added the users to the project'})
+    } catch(e){
+        return res.status(500).send({message: 'Server couldnt add the users the project...'})
+    }
+}
 async function updateMemberRole(req: any, res: any){
     let userTeamRoleCombo: any = []
     let targetUser: any = null;
     let targetProjectID: any = null;
     let currentUserProjectRole: any = null;
     let targetUserProjectRole: any = null;
-
     try{
         userTeamRoleCombo = consumeCookie(req.headers.cookie, consumeCookieFlags.tokenUserTeamRoleIdFlag);
         targetUser = await users.getUserByNameDiscriminator(req.body.username, req.body.discriminator)
@@ -153,7 +210,6 @@ async function updateMemberRole(req: any, res: any){
     }catch(e){
         return res.status(500).send({message: 'Server couldnt find role information...'})
     }
-
     //if the currentUser has higher perms than the targetUser, then edit the target user's role as requested
     if(currentUserProjectRole[0].role_id < targetUserProjectRole[0].role_id){
         try{
@@ -163,7 +219,7 @@ async function updateMemberRole(req: any, res: any){
             return res.status(500).send({message: 'Server couldnt update role...'})
         }
     } else {
-        return res.status(400).send({message: 'You dont have the ability to edit that users role on this project'})
+        return res.status(400).send({message: 'You dont have the ability to edit that users role on this project...'})
     }
 }
 async function getUsersOnTeam(req: any, res: any){
@@ -176,9 +232,35 @@ async function getUsersOnTeam(req: any, res: any){
     }
 }
 async function removeMember(req: any, res: any) {
-    console.log(req.body)
-
-    //if the user is 
+    let userTeamRoleCombo: userTeamRoleCombo = consumeCookie(req.headers.cookie, consumeCookieFlags.tokenUserTeamRoleIdFlag);
+    let userProjectRoleId: number | null = null;
+    let targetUserProjectRoleId: number | null = null;
+    let targetProjectId: number | null = null;
+    let targetUserId: number | null = null;
+    try{
+        let targetProjectIdPacket = await projects.getProjectIdByTeamIdAndProjectName(userTeamRoleCombo.teamID, req.body.project)
+        targetProjectId = targetProjectIdPacket.project_id
+        let userProjectRoleIdPacket = await projects.getRoleByUserIdProjectId(userTeamRoleCombo.userID, targetProjectId)
+        userProjectRoleId = userProjectRoleIdPacket[0].role_id
+        let targetUserProjectRoleIdPacket = await projects.getRoleByUsernameDiscriminatorProjectId(req.body.username, req.body.discriminator, targetProjectId)
+        targetUserProjectRoleId = targetUserProjectRoleIdPacket[0].role_id
+        let targetUserIdPacket = await users.getUserByNameDiscriminator(req.body.username, req.body.discriminator)
+        targetUserId = targetUserIdPacket.user_id
+    }catch(e){
+        return res.status(500).send({message: 'Server couldnt check authorization levels...'})
+    }
+    //if the user has a role_id less than the target user, they can remove the target user as a member of the project
+    if(userProjectRoleId! < targetUserProjectRoleId!){
+        try{
+            await projects.transactionRemoveTargetUserFromProject(targetUserId, targetProjectId)
+            return res.status(200).send({message: 'Removed member from the project'})
+        }catch(e){
+            console.log(e)
+            return res.status(500).send({message: 'Server couldnt remove member from the project...'})
+        }
+    } else{ 
+        return res.status(403).send({message: `You are not authorized to remove ${req.body.username} #${req.body.discriminator} from the ${req.body.project} project...`})
+    }
 }
 
 
@@ -189,5 +271,6 @@ module.exports = {
     getRelatedMemberDetails,
     updateMemberRole,
     getUsersOnTeam,
-    removeMember
+    removeMember,
+    addListOfMembersToProject
 }
