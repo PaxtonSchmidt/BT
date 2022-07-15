32683171188a9f5c5167d4e27a3b9b640eef054e
PaxtonSchmidt <67126427+PaxtonSchmidt@users.noreply.github.com>
2022-07-15 10:03:57 -0400

    added the invite to team dropdown container

---
+++ client/src/Components/Layout/Pages/ManageTeamPage/inviteToTeamFormContainer.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+import InviteToTeamForm from '../../../Library/Forms/inviteToTeamForm'
+
+export default function InviteToTeamForContainer(){
+    const [isExtended, setIsExtended] = useState(false)
+
+    function handleOpenProjectForm(){
+        if(isExtended === false){
+            document.getElementById('pageContentContainer')!.classList.add('FormContainerTransition');
+            document.getElementById('bi-arrow-bar-down')!.classList.add('twist');       
+            document.getElementById('bi-arrow-bar-down')!.classList.add('noPadding');       
+        } else{
+            document.getElementById('pageContentContainer')!.classList.remove('FormContainerTransition');
+            document.getElementById('bi-arrow-bar-down')!.classList.remove('twist');        
+        }
+        setIsExtended(!isExtended)
+    }
+    return (
+        <div id='pageContentContainer' className='FormContainer fadeIn' style={{display: 'flex', alignItems: 'center'}}>
+            <InviteToTeamForm />
+            <div className='formLoadAnim'>
+                <h1 onClick={handleOpenProjectForm} className='button svgSibling' style={{width: '100%', cursor: 'pointer', backgroundColor: 'transparent', marginBottom: '0px', marginTop: '3px', marginRight:'0px'}}>Invite Someone</h1>
+                <svg onClick={handleOpenProjectForm} style={{paddingRight: '0px'}} id='bi-arrow-bar-down' xmlns="http://www.w3.org/2000/svg" width="100%" height="25" fill="currentColor" className="bi bi-arrow-bar-down" viewBox="0 0 16 16">
+                    <path fillRule="evenodd" d="M1 3.5a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13a.5.5 0 0 1-.5-.5zM8 6a.5.5 0 0 1 .5.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 0 1 .708-.708L7.5 12.293V6.5A.5.5 0 0 1 8 6z"/>
+                </svg> 
+            </div>
+        </div>
+    )
+}
