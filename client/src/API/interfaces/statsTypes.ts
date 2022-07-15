bf9db78b531a12202351c5c30b52c3b786a987b9
PaxtonSchmidt <67126427+PaxtonSchmidt@users.noreply.github.com>
2022-07-15 10:01:35 -0400

    defined the types for the projectStats object that will be dynamically fed into the pie chart

---
+++ client/src/API/interfaces/statsTypes.ts
@@ -0,0 +1,32 @@
+export interface StatusStats {
+    Unassigned: number,
+    Open: number,
+    SolutionProposed: number,
+    SolutionRejected: number,
+    Closed: number
+}
+export interface PriorityStats{
+    Low: number,
+    Medium: number, 
+    High: number
+}
+export interface Assignee {
+    username: string,
+    discriminator: number,
+    amount: number
+}
+export interface ProjectStats{
+    ticketStatusStats: StatusStats,
+    ticketPriorityStats: PriorityStats,
+    ticketAssigneeStats: Assignee[]
+}
+type AllProjectsStats = Omit<ProjectStats, 'ticketAssigneeStats'>
+export interface Project {
+    project_id: null | number,
+    project_name: string,
+    projectStats: ProjectStats
+}
+export default interface ComposedStats {
+    projects: Project[],
+    allProjectsStats: AllProjectsStats
+}
