367064261e88b59b3c0e569390bbb7a370157546
PaxtonSchmidt <67126427+PaxtonSchmidt@users.noreply.github.com>
2022-07-15 10:01:45 -0400

    added log

--- client/src/API/Requests/Invites/PostInviteToTeam.ts
+++ client/src/API/Requests/Invites/PostInviteToTeam.ts
@@ -6,6 +6,7 @@ export default async function postInviteToTeam(potentialTeammates: any) {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(potentialTeammates)
-    })
+    }).then(res => res.json())
+    console.log(response)
     return response.status
 }
