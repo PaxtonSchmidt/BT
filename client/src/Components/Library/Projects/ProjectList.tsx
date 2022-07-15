51a61f732fad7a52c45f8e10bd2c1b1b762c3ee7
PaxtonSchmidt <67126427+PaxtonSchmidt@users.noreply.github.com>
2022-07-15 10:50:31 -0400

    adds some css to the all button during intial render so the default button is highlighted

--- client/src/Components/Library/Projects/ProjectList.tsx
+++ client/src/Components/Library/Projects/ProjectList.tsx
@@ -7,9 +7,12 @@ import { State } from '../../../Redux/reducers';
 import ProjectListItem from './ProjectListItem';
 
 function ProjectList() {
+    const dispatch = useDispatch();
+    const { updateFocusedProject } = bindActionCreators(FocusedProjectActionCreators, dispatch)
+    const focusedProjectState = useSelector((state: State) => state.focusedProject)
     const [filterToLeadingOnly, setFilterToLeadingOnly]= useState(false)
     const sessionState = useSelector((state: State) => state.session)
-    let projectsList: any = ['All'] 
+    let projectsList: any = [] 
     let currentUser = {
         username: sessionState.currentUser?.username,
         discriminator: sessionState.currentUser?.discriminator
@@ -32,8 +35,16 @@ function ProjectList() {
     } else {
         sessionState.currentTeam?.projects.forEach((project: any) => projectsList.push(project.name))
     }
-    
- 
+
+    let projectAllName = 'All'
+    if(focusedProjectState.name === projectAllName){
+        document.getElementById(projectAllName)?.classList.add('selectedProjectInList')
+        document.getElementById(projectAllName)?.classList.remove('scaleYonHover')
+    } else {
+        document.getElementById(projectAllName)?.classList.remove('selectedProjectInList')
+        document.getElementById(projectAllName)?.classList.add('scaleYonHover')
+    }
+
     function checkView(){
         if(filterToLeadingOnly === true){
             return 'Viewing Projects You Lead'
@@ -46,29 +57,29 @@ function ProjectList() {
 
     if(!projectsList){
         return<></>
+    }else if(projectsList.length === 0 && filterToLeadingOnly === false){
+        return <h4 style={{color: '#ffffff', marginTop: '5px', marginBottom: '5px', height: '20px'}}>Looks like you haven't been assigned to any projects yet</h4>
     }else{
-
         function handleWheel(e: any){
             let projectList = document.getElementById('projectList')
             if(projectList){    
                 let width = projectList.clientWidth
-                
                 projectList!.scrollLeft += e.deltaY;
                 let timer: any = console.log(projectList.scrollLeft)
-                // console.log(width)
-                // console.log(projectList.scrollWidth)
             }      
         }
         
         //needs pagination
         return (
             <div onWheel={handleWheel} id='projectList'  className='sideScrollList componentGlow'>
+                <div onClick={() => updateFocusedProject({name: 'All'})} className='sideScrollListItem scaleYonHover fadeIn selectedProjectInList' id={projectAllName}>
+                    All
+                </div>
                 {projectsList!.map((project: any) =>
                     <ProjectListItem key={project} name={project} />
                 )}
                 <button className='filterProjectsToggle scaleYonHover' onClick={() => setFilterToLeadingOnly(!filterToLeadingOnly)}>{WhichView}</button>
             </div>
-            
         )
     }
 }
