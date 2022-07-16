88722a7d0f977ec57ed5599de7d6488b00fe831d
PaxtonSchmidt <67126427+PaxtonSchmidt@users.noreply.github.com>
2022-07-16 18:39:23 -0400

    added socket.io boilerplate

--- server/app.ts
+++ server/app.ts
@@ -1,11 +1,32 @@
 import authenticateRequest from "./API/Middleware/authenticateRequest";
-
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const http = require('http')
+const { Server } = require('socket.io')
+app.use(cors());
+
+const server = http.createServer(app)
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ['GET', 'POST']
+    }
+})
+
+server.listen('4000', () => {
+    console.log('server started on port 4000');
+})
+
+
+io.on("connection", (socket: any) => {
+    console.log(socket.id)
+    socket.conn.on("upgrade", () =>{
+        console.log('upgraded')
+    })
+})
 
 app.use(express.json());
-app.use(cors());
 app.use('/', require('./API/Routes/AuthenticationRoutes/authenticationRoute'));
 app.use('/signup/', require('./API/Routes/signUpRoute'));
 
@@ -25,6 +46,3 @@ app.use('/tickets/', authenticateRequest,
 app.use('/projects/', authenticateRequest,
     require('./API/Routes/projectRoute'));
     
-app.listen('4000', () => {
-    console.log('server started on port 4000');
-})
