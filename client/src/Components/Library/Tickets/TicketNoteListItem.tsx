41ee7ad3ca67cd8e23ef6a44f69285dae0e65380
PaxtonSchmidt <67126427+PaxtonSchmidt@users.noreply.github.com>
2022-07-18 23:20:24 -0400

    wrote the most convoluted code possible to format the dates in the ticket chat box

--- client/src/Components/Library/Tickets/TicketNoteListItem.tsx
+++ client/src/Components/Library/Tickets/TicketNoteListItem.tsx
@@ -7,19 +7,46 @@ interface Props {
 
 export default function TicketNoteListItem(props: Props){
     let isPm: boolean = false
+    let etcDifferenceToUTC: number = 4
     let isSingleDigitMinutes: boolean = false
-    let date = props.note.date_created
+    let date = parseInt(props.note.date_created.substring(5,10).replace('-', ''))
+    let hours = parseInt(props.note.date_created.toLocaleString().substring(11, 13))
+    let minutes = parseInt(props.note.date_created.toLocaleString().substring(14, 16))
+    // console.log(`${props.note.date_created} ${hours} ${minutes}`)
+    // console.log(24 - (hours - etcDifferenceToUTC))
+    if(hours < etcDifferenceToUTC){
+        hours = 24 + (hours - etcDifferenceToUTC)
+        date = date - 1
+    } else{
+        hours = hours - etcDifferenceToUTC
+    }
+    if(hours > 12){
+        hours = hours % 12
+        isPm = true
+    } 
+    if(hours === 0){
+        hours = 12
+    }
+    if(minutes < 10){
+        isSingleDigitMinutes = true
+    }
 
-    // if(hours > 12){
-    //     hours = hours % 12
-    //     isPm = true
-    // }
-    // if(hours === 0){
-    //     hours = 12
-    // }
-    // if(minutes < 10){
-    //     isSingleDigitMinutes = true
-    // }
+    let zero=''
+    if(isSingleDigitMinutes){
+        zero = '0'
+    }
+    let pm = ''
+    if(isPm){
+        pm = 'pm'
+    } else {
+        pm = 'am'
+    }
+
+    let dateArray = date.toString().split('').reverse().join().replace(/,/g,'')
+    let dateDay = dateArray.substring(0, 2)
+    let dateMonth = dateArray.replace(dateDay, '').split('').reverse().join().replace(/,/g,'')
+    
+    dateDay = dateDay.split('').reverse().join().replace(/,/g,'')
 
     return (
         <div className='noteListItem'>
@@ -31,9 +58,13 @@ export default function TicketNoteListItem(props: Props){
                 <p style={{margin: '0px', height: 'fit-content'}}>
                     {props.note.body}
                     <span className='time'>
-                        {/* {` ${hours}:${isSingleDigitMinutes ? '0' : ''}${minutes}${isPm ? 'pm' : 'am'}`}
-                        {days} */}
-                        {date}
+                        {` ${hours}`}
+                    </span>
+                    <span className='time'>
+                        {`:${zero}${minutes}${pm}`}
+                    </span>
+                    <span className='time'>
+                        {`\u00a0${dateMonth}/${dateDay}`}
                     </span>
                 </p>
             </div>
