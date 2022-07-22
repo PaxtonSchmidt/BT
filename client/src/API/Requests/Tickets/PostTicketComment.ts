18c0a0c92038a4f92cab5368104de1086d54e8df
PaxtonSchmidt <67126427+PaxtonSchmidt@users.noreply.github.com>
2022-07-21 22:56:00 -0400

    return the status and body of  a response in one object

--- client/src/API/Requests/Tickets/PostTicketComment.ts
+++ client/src/API/Requests/Tickets/PostTicketComment.ts
@@ -5,11 +5,7 @@ export default async function postTicketComment(comment: string, ticket: any) {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({comment: comment, ticketID: ticket.ticket_id, projectID: ticket.project_id})
-    }).then(resonse => {if(resonse.status !== 200){
-                            return 'Error'
-                        } else{
-                            return resonse.json()
-                        }})
+    }).then(r =>  r.json().then(data => ({status: r.status, body: data})))
 
     return response
 }
