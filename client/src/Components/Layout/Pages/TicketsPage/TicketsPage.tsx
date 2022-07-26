8601c1ff34b0122ed5bd2ac4e4391f716a08ea44
PaxtonSchmidt <67126427+PaxtonSchmidt@users.noreply.github.com>
2022-07-25 22:47:39 -0400

    lifted ticket form state to the form container

--- client/src/Components/Layout/Pages/TicketsPage/TicketsPage.tsx
+++ client/src/Components/Layout/Pages/TicketsPage/TicketsPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 import { State } from '../../../../Redux/reducers';
@@ -14,13 +14,15 @@ interface Props {
 
 export default function TicketsPage({ isTeamSelected } : Props) {
     const loginState = useSelector((state: State) => state.login)
+    const [isExtended, setIsExtended] = useState(false)
     
+    let isFormContainerTransition: string = isExtended === true ? 'FormContainerTransition' : ''
     if(loginState === 1) {
         if(isTeamSelected === true){
             return(
                     <div className='overflow'>
-                        <div id='pageContentContainer' className='pageContentContainer' >
-                            <TicketFormContainer/>
+                        <div id='pageContentContainer' className={`pageContentContainer ${isFormContainerTransition}`} >
+                            <TicketFormContainer isExtended={isExtended} setIsExtended={setIsExtended}/>
                             <CommentsAndDetails />
                         </div>
                             <TicketList />
