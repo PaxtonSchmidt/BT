b9a44bd8df08b1112cca82288015ed0191ab4ef8
PaxtonSchmidt <67126427+PaxtonSchmidt@users.noreply.github.com>
2022-07-25 22:54:34 -0400

    fixed translate ticket status to represent new status definitions

---
+++ client/src/Services/translateTicketStatus.ts
@@ -0,0 +1,46 @@
+export const statusTranslation = {
+    translateTicketStatus(word:  string){
+        switch (word){
+            case 'Unassigned':
+                return 1
+                break;
+            case 'Assigned': 
+                return 2
+                break;
+            case 'Investigating': 
+                return 3
+                break;
+            case 'Reviewing':
+                return 4
+                break;
+            case 'Closed':
+                return 5
+                break;
+            default: 
+                return 'ERROR IN TRANSLATING TICKET STATUS'
+        }
+    },
+    translateTicketStatusBack(number: number){
+        switch (number){
+            case 1:
+                return 'Unassigned'
+                break;
+            case 2: 
+                return 'Assigned'
+                break;
+            case 3: 
+                return 'Investigating'
+                break;
+            case 4:
+                return 'Reviewing'
+                break;
+            case 5:
+                return 'Closed'
+                break;
+            default: 
+                return 'ERROR IN TRANSLATING TICKET STATUS'
+        }
+    }
+}
+
+
