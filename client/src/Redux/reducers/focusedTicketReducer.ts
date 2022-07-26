f1b13f8723a57af1d65e32b3d48b289a94ef7c47
PaxtonSchmidt <67126427+PaxtonSchmidt@users.noreply.github.com>
2022-07-25 22:53:02 -0400

    added a reducer to update the focused ticket when the user updated a ticket

--- client/src/Redux/reducers/focusedTicketReducer.ts
+++ client/src/Redux/reducers/focusedTicketReducer.ts
@@ -7,6 +7,16 @@ const focusedTicketReducer = (state: any = initialState, action: Action) => {
     switch(action.type){
         case focusedTicketActionType.UPDATE:
             return action.payload;
+        case focusedTicketActionType.EDIT:
+            let editedTicket = {...state}
+            console.log(state)
+            console.log(editedTicket)
+            editedTicket.assignee_username= action.payload.assignee.username
+            editedTicket.assignee_user_discriminator= action.payload.assignee.discriminator
+            editedTicket.description= action.payload.description
+            editedTicket.priority= action.payload.priority
+            editedTicket.resolution_status= action.payload.resolution_status
+            return editedTicket
         default: 
             return state; 
     }
