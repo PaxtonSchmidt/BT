c07c983781e7efab97282192f7ffb36dd0dd91e3
PaxtonSchmidt <67126427+PaxtonSchmidt@users.noreply.github.com>
2022-07-27 22:31:46 -0400

    added all the boilerplate for redux sessionState so i can add members to projects, remove members from projects and updateMemberRoles in a project

--- client/src/Redux/action-creators/sessionActionCreators.ts
+++ client/src/Redux/action-creators/sessionActionCreators.ts
@@ -1,6 +1,7 @@
 import { Dispatch } from "redux"
 import { sessionActionType } from "../action-types/sessionActionType"
 import { Action } from "../actions/sessionAction"
+import { ProjectMember } from "../interfaces/member"
 import { Session } from "../interfaces/session"
 
 export const updateSession = (session: Session) => {
@@ -8,6 +9,30 @@ export const updateSession = (session: Session) => {
         dispatch({
             type: sessionActionType.UPDATE,
             payload: session
+        })
+    }
+}
+export const addMembersToAProjectInSession = (members: ProjectMember[]) => {
+    return (dispatch: Dispatch<Action>) => {
+        dispatch({
+            type: sessionActionType.ADD_MEMBERS_TO_PROJECT,
+            payload: members
+        })
+    }
+}
+export const removeMemberFromProjectInSession = (member: ProjectMember) => {
+    return (dispatch: Dispatch<Action>) => {
+        dispatch({
+            type: sessionActionType.REMOVE_MEMBER_FROM_PROJECT,
+            payload: member
+        })
+    }
+}
+export const updateProjectMemberRoleInSession = (member: ProjectMember) => {
+    return (dispatch: Dispatch<Action>) => {
+        dispatch({
+            type: sessionActionType.UPDATE_PROJECT_MEMBER_ROLE,
+            payload: member
         })
     }
 }
--- client/src/Redux/actions/sessionAction.ts
+++ client/src/Redux/actions/sessionAction.ts
@@ -1,4 +1,5 @@
 import { sessionActionType } from "../action-types/sessionActionType"
+import { ProjectMember } from "../interfaces/member"
 import { Session } from "../interfaces/session"
 
 interface UpdateAction {
@@ -6,7 +7,22 @@ interface UpdateAction {
     payload: Session
 }
 
-export type Action = UpdateAction
+interface addMembersToAProjectAction {
+    type: sessionActionType.ADD_MEMBERS_TO_PROJECT,
+    payload: ProjectMember[]
+}
+
+interface removeMemberFromProjectInSession{
+    type: sessionActionType.REMOVE_MEMBER_FROM_PROJECT,
+    payload: ProjectMember
+}
+
+interface updatedProjectMemberRole{
+    type: sessionActionType.UPDATE_PROJECT_MEMBER_ROLE,
+    payload: ProjectMember
+}
+
+export type Action = UpdateAction | addMembersToAProjectAction | removeMemberFromProjectInSession | updatedProjectMemberRole
 
 
 
