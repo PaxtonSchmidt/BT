500e739b4e6a6122e256a19af505c1d60abb22eb
PaxtonSchmidt <67126427+PaxtonSchmidt@users.noreply.github.com>
2022-07-27 22:26:30 -0400

    edit ticket checks if the assignee is null and formats it for the server before sending the request

--- client/src/API/Requests/Tickets/PutEditTicket.ts
+++ client/src/API/Requests/Tickets/PutEditTicket.ts
@@ -6,8 +6,8 @@ export default async function putEditTicket(ticket: Ticket, ticket_id: number, p
         target_ticket_id: ticket_id,
         project_id: project_id,
         priority: ticket.priority,
-        assignee_username: ticket.assignee.username,
-        assignee_discriminator: ticket.assignee.discriminator,
+        assignee_username: ticket.assignee === '' ? '' : ticket.assignee.username,
+        assignee_discriminator: ticket.assignee === '' ? null : ticket.assignee.discriminator,
         description: ticket.description,
         resolution_status: ticket.resolution_status
     }
