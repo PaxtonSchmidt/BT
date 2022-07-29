d4d20c6ee8af6a5f0ca3425de7650ec52c90a2e6
PaxtonSchmidt <67126427+PaxtonSchmidt@users.noreply.github.com>
2022-07-28 22:28:34 -0400

    added teammates state action types

--- client/src/Redux/action-types/teammatesActionType.ts
+++ client/src/Redux/action-types/teammatesActionType.ts
@@ -1,3 +1,5 @@
 export enum teammatesActionType {
-    UPDATE = 'updateTeammates'
+    UPDATE = 'updateTeammates',
+    UPDATE_TEAMMATE_ROLE = 'UPDATE_TEAMMATE_ROLE',
+    REMOVE_TEAMMATE = 'REMOVE_TEAMMATE'
 }
