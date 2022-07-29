fc4cdc40c449d19c1d11ebd89199004d392cbe64
PaxtonSchmidt <67126427+PaxtonSchmidt@users.noreply.github.com>
2022-07-28 22:30:10 -0400

    added teammates reducers

--- client/src/Redux/reducers/teammatesReducer.ts
+++ client/src/Redux/reducers/teammatesReducer.ts
@@ -5,9 +5,21 @@ import { Teammate } from "../interfaces/teammate";
 const initialState = {}; 
 
 const teammatesReducer = (state: any = initialState, action: Action) => {
+    let newState = []
+    let targetTeammateIDX: number | null = null
     switch(action.type){
         case teammatesActionType.UPDATE:
             return action.payload;
+        case teammatesActionType.UPDATE_TEAMMATE_ROLE:
+            newState = [...state]
+            targetTeammateIDX = newState.findIndex((teammate: Teammate) =>{return teammate.username === action.payload.username && teammate.discriminator === action.payload.discriminator})
+            newState.splice(targetTeammateIDX, 1, action.payload)
+            return newState 
+        case teammatesActionType.REMOVE_TEAMMATE:
+            newState = [...state]
+            targetTeammateIDX = newState.findIndex((teammate: Teammate) =>{return teammate.username === action.payload.username && teammate.discriminator === action.payload.discriminator})
+            newState.splice(targetTeammateIDX, 1)
+            return newState
         default: 
             return state; 
     }
