1de47fdfc6cb96c09fca243bff189ce7f9bf193a
PaxtonSchmidt <67126427+PaxtonSchmidt@users.noreply.github.com>
2022-07-31 12:30:14 -0400

    remove teammate from sessions projects

--- client/src/Redux/action-creators/sessionActionCreators.ts
+++ client/src/Redux/action-creators/sessionActionCreators.ts
@@ -3,6 +3,7 @@ import { sessionActionType } from "../action-types/sessionActionType"
 import { Action } from "../actions/sessionAction"
 import { ProjectMember } from "../interfaces/member"
 import { Project, Session } from "../interfaces/session"
+import { Teammate } from "../interfaces/teammate"
 
 export const updateSession = (session: Session) => {
     return (dispatch: Dispatch<Action>) => {
@@ -41,6 +42,14 @@ export const addProjectToSession = (project: Project) => {
         dispatch({
             type: sessionActionType.ADD_PROJECT_TO_SESSION,
             payload: project
+        })
+    }
+}
+export const removeTeammateFromSessionProjects = (teammate: Teammate) => {
+    return (dispatch: Dispatch<Action>) => {
+        dispatch({
+            type: sessionActionType.REMOVE_TEAMMATE_FROM_SESSION_PROJECTS,
+            payload: teammate
         })
     }
 }
