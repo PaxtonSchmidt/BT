8b8bc398a1893df565e3df6e663a16394f2a3f4d
PaxtonSchmidt <67126427+PaxtonSchmidt@users.noreply.github.com>
2022-08-01 00:15:02 -0400

    Wrote the code that sorts tickets for the ticket list

---
+++ client/src/Components/ComponentInterfaces/sorts.ts
@@ -0,0 +1,57 @@
+import { ticket } from "./ticket"
+
+export interface SortTypes {
+    project: string,
+    status: string,
+    priority: string,
+    title: string
+}
+
+export const sorts: SortTypes = {project: 'project', status: 'status', priority: 'priority', title: 'title'}
+
+export interface SortActions{
+    sortByProject: (tickets: ticket[])=>ticket[],
+    sortByStatus: (tickets: ticket[])=>ticket[],
+    sortByPriority: (tickets: ticket[])=>ticket[],
+    sortByTitle: (tickets: ticket[])=>ticket[]
+}
+
+export const sortAction: SortActions = {
+    sortByProject: (tickets: ticket[])=>{
+        tickets.sort(projectCompare)
+        return tickets
+    },
+    sortByStatus: (tickets: ticket[])=>{
+        tickets.sort(statusCompare)
+        return tickets
+    },
+    sortByPriority: (tickets: ticket[])=>{
+        tickets.sort(priorityCompare)
+        return tickets
+    },
+    sortByTitle: (tickets: ticket[])=>{
+        tickets.sort(titleCompare)
+        return tickets
+    },
+}
+
+function projectCompare(i: ticket, j: ticket){
+    return (i.project_name < j.project_name) ? -1 :
+    (j.project_name < i.project_name) ? 1 : 
+    0
+}
+function priorityCompare(i: ticket, j: ticket){
+    return (i.priority < j.priority) ? -1 :
+    (j.priority < i.priority) ? 1 : 
+    0
+}
+function statusCompare(i: ticket, j: ticket){
+    return (i.resolution_status < j.resolution_status) ? -1 :
+    (j.resolution_status < i.resolution_status) ? 1 : 
+    0
+}
+function titleCompare(i: ticket, j: ticket){
+    return (i.title < j.title) ? -1 :
+    (j.title < i.title) ? 1 : 
+    0
+}
