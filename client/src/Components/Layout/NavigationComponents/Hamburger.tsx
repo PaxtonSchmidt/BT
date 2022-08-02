5fc5c3c5c5e2a84272b034c37045ccda7fc99b84
PaxtonSchmidt <67126427+PaxtonSchmidt@users.noreply.github.com>
2022-08-02 17:17:44 -0400

    added sidebar extension and preference saves in localstorage

--- client/src/Components/Layout/NavigationComponents/Hamburger.tsx
+++ client/src/Components/Layout/NavigationComponents/Hamburger.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { SetStateAction } from 'react';
 import cheeburger from '../../Images/Icons/list.svg';
 
-export default function Hamburger() {
-    
+interface Props{
+    setIsSideBarExpanded: React.Dispatch<React.SetStateAction<boolean>>,
+    isSideBarExpanded: boolean
+}
+
+export default function Hamburger(props: Props) {
+    function handleSetIsSideBarExpanded(){
+        let isExtended = !props.isSideBarExpanded
+        window.localStorage.setItem("sbEX", isExtended.toString())
+        props.setIsSideBarExpanded(!props.isSideBarExpanded)
+    }
     return(
-        <div className='hamburgerContainer fadeIn'>
+        <div className='hamburgerContainer fadeIn scaleYonHover' onClick={()=>handleSetIsSideBarExpanded()}>
             <img src={cheeburger} alt='open navigation menu' className='hamburger longFadeIn' />
         </div> 
     )
