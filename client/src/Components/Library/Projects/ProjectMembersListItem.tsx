f8e4f0d58368fce9cd759db8126ea3d9502d697f
PaxtonSchmidt <67126427+PaxtonSchmidt@users.noreply.github.com>
2022-08-03 22:20:29 -0400

    prettier doing damage

--- client/src/Components/Library/Projects/ProjectMembersListItem.tsx
+++ client/src/Components/Library/Projects/ProjectMembersListItem.tsx
@@ -1,45 +1,85 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { FocusedMemberActionCreators } from '../../../Redux';
 import { State } from '../../../Redux/reducers';
-import {translateRole} from '../../../Services/translateRole'
+import { translateRole } from '../../../Services/translateRole';
 
-interface Props{
-    member: any;
+interface Props {
+  member: any;
 }
 
 export default function ProjectMembersListItem(props: Props) {
-    const dispatch = useDispatch();
-    const { updateFocusedMember } = bindActionCreators(FocusedMemberActionCreators, dispatch)
-    const focusedProjectState = useSelector((state: State) => state.focusedProject)
-    let role = translateRole.TranslateProjectRole(props.member.role_id)
-    
-    function handleSelect(){
-        if(focusedProjectState.name !== 'All'){
-            return updateFocusedMember(props.member)
-        }
+  const dispatch = useDispatch();
+  const { updateFocusedMember } = bindActionCreators(
+    FocusedMemberActionCreators,
+    dispatch
+  );
+  const focusedProjectState = useSelector(
+    (state: State) => state.focusedProject
+  );
+  let role = translateRole.TranslateProjectRole(props.member.role_id);
+
+  function handleSelect() {
+    if (focusedProjectState.name !== 'All') {
+      return updateFocusedMember(props.member);
     }
-    return (
-        <div className='ListContainer scaleYonHover hoverGrey' onClick={handleSelect} style={{marginLeft: '0px'}}>
-            <div  className={`listItem listRow memberRow ${focusedProjectState.name === 'All' && 'defaultCursor'}`} style={{justifyContent: 'space-between', overflowX:'hidden', marginLeft: '0px'}}>
-                <div className='memberListRowSection' style={{display:'flex', textAlign: 'center', alignContent: 'bottom'}}>
-                    <span className='rowItem username' style={{marginLeft:'5px', display: 'inline-block', width: 'fit-content', maxWidth: '160px', overflowX: 'hidden', textOverflow: 'ellipsis'}}>
-                        {props.member.username } 
-                    </span>
-                    <span className='rowItem discriminator' style={{display: 'inline-block', width: 'fit-content', marginTop: 'auto'}}>
-                      #{props.member.discriminator}
-                    </span>
-                    {focusedProjectState.name !== 'All' && <span className='showDetailsHighlight'>{`Show details`}</span>}
-                </div>
-                
-                <div className='memberListRowSection' style={{textAlign: 'center', width: '100px'}}>
-                    <span className='rowItem'>
-                        {focusedProjectState.name === 'All' ? <></> :
-                        <>{role}</>}
-                    </span>
-                </div>
-            </div>
+  }
+  return (
+    <div
+      className='ListContainer scaleYonHover hoverGrey'
+      onClick={handleSelect}
+      style={{ marginLeft: '0px' }}>
+      <div
+        className={`listItem listRow memberRow ${
+          focusedProjectState.name === 'All' && 'defaultCursor'
+        }`}
+        style={{
+          justifyContent: 'space-between',
+          overflowX: 'hidden',
+          marginLeft: '0px',
+        }}>
+        <div
+          className='memberListRowSection'
+          style={{
+            display: 'flex',
+            textAlign: 'center',
+            alignContent: 'bottom',
+          }}>
+          <span
+            className='rowItem username'
+            style={{
+              marginLeft: '5px',
+              display: 'inline-block',
+              width: 'fit-content',
+              maxWidth: '160px',
+              overflowX: 'hidden',
+              textOverflow: 'ellipsis',
+            }}>
+            {props.member.username}
+          </span>
+          <span
+            className='rowItem discriminator'
+            style={{
+              display: 'inline-block',
+              width: 'fit-content',
+              marginTop: 'auto',
+            }}>
+            #{props.member.discriminator}
+          </span>
+          {focusedProjectState.name !== 'All' && (
+            <span className='showDetailsHighlight'>{`Show details`}</span>
+          )}
         </div>
-    )
+
+        <div
+          className='memberListRowSection'
+          style={{ textAlign: 'center', width: '100px' }}>
+          <span className='rowItem'>
+            {focusedProjectState.name === 'All' ? <></> : <>{role}</>}
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 }
