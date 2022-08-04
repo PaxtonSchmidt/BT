62f95c5a512a8013c304449ca61402fb21671182
PaxtonSchmidt <67126427+PaxtonSchmidt@users.noreply.github.com>
2022-08-03 22:16:29 -0400

    added notes for projects

---
+++ client/src/API/interfaces/ProjectNote.ts
@@ -0,0 +1,9 @@
+export interface ProjectNote {
+    comment_id: number;
+    author_username: string;
+    author_discriminator: number;
+    body: string;
+    project_id: number;
+    date_created: string;
+  }
+  
