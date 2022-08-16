97a7811c7ba11e3ca83fbbb16fda24d936063d66
PaxtonSchmidt <67126427+PaxtonSchmidt@users.noreply.github.com>
2022-08-15 22:00:16 -0400

    make it clean

--- client/src/Components/Library/Teams/TeamMemberList.tsx
+++ client/src/Components/Library/Teams/TeamMemberList.tsx
@@ -44,7 +44,7 @@ export default function TeammateList() {
       <>
         <div
           className='listRow fadeIn'
-          style={{ marginTop: '0px', backgroundColor: '#222222', justifyContent: 'center' , width: 'calc(100%-20px)' }}
+          style={{ marginTop: '0px', backgroundColor: '#222222', justifyContent: 'center' , padding: '0px', paddingLeft: '0px !important', paddingRight: '0px !important'}}
         >
           <div
             className='memberListRowSection fadeIn'
